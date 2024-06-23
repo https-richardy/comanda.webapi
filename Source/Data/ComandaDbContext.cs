2e82498f6bb28b1bc95c71c6cfffd42334db9308
https-richardy <code.richardy@gmail.com>
2024-06-23 20:32:51 -0300

    add DbSets for entities in ComandaDbContext.

--- Source/Data/ComandaDbContext.cs
+++ Source/Data/ComandaDbContext.cs
@@ -1,6 +1,13 @@
 namespace Comanda.WebApi.Data;
 
-public sealed class ComandaDbContext(DbContextOptions options) : DbContext(options)
+public sealed class ComandaDbContext(DbContextOptions options) : IdentityDbContext<Account>(options)
 {
-
+    public DbSet<Establishment> Establishments { get; set; }
+    public DbSet<EstablishmentOwner> EstablishmentOwners { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Address> Addresses { get; set; }
 }
