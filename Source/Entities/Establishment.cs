701b5b4b77720fea2dfb5383190b76f97912438d
https-richardy <code.richardy@gmail.com>
2024-06-23 19:10:36 -0300

    added Establishment entity to represent establishments in the system.

---
+++ Source/Entities/Establishment.cs
@@ -0,0 +1,17 @@
+namespace Comanda.WebApi.Entities;
+
+public sealed class Establishment : Entity
+{
+    public string Name { get; set; }
+
+    public Address Address { get; set; }
+    public ICollection<Product> Products { get; set; } = [];
+
+    public Establishment()
+    {
+        /*
+            Default parameterless constructor included due to Entity Framework Core not setting navigation properties
+            when using constructors. For more information, see: https://learn.microsoft.com/pt-br/ef/core/modeling/constructors
+        */
+    }
+}
