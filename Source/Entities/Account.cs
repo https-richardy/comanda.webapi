e3e91befafb6afbee730e250cc706ff5d4744e70
https-richardy <code.richardy@gmail.com>
2024-06-23 20:05:01 -0300

    add Account class inheriting from IdentityUser for authentication.

---
+++ Source/Entities/Account.cs
@@ -0,0 +1,12 @@
+namespace Comanda.WebApi.Entities;
+
+public sealed class Account : IdentityUser
+{
+    public Account()
+    {
+        /*
+            Default parameterless constructor included due to Entity Framework Core not setting navigation properties
+            when using constructors. For more information, see: https://learn.microsoft.com/pt-br/ef/core/modeling/constructors
+        */
+    }
+}
