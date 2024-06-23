18a1cc27600b8e72d0d3c5663bf1ee3d1c7055ac
https-richardy <code.richardy@gmail.com>
2024-06-23 19:30:42 -0300

    add Order entity with total calculation, date, establishment, items, and status."

---
+++ Source/Entities/Order.cs
@@ -0,0 +1,19 @@
+namespace Comanda.WebApi.Entities;
+
+public sealed class Order : Entity
+{
+    public decimal Total => OrderItems.Sum(item => item.Price);
+    public DateTime Date { get; set; }
+
+    public Establishment Establishment { get; set; }
+    public ICollection<OrderItem> OrderItems { get; set; } = [];
+    public EOrderStatus Status { get; set; }
+
+    public Order()
+    {
+        /*
+            Default parameterless constructor included due to Entity Framework Core not setting navigation properties
+            when using constructors. For more information, see: https://learn.microsoft.com/pt-br/ef/core/modeling/constructors
+        */
+    }
+}
