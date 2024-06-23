065c981c826328cdcabeadd3d6d3b995c445018e
https-richardy <code.richardy@gmail.com>
2024-06-23 18:59:55 -0300

    added Category entity, to represent snack/product categories

---
+++ Source/Entities/Category.cs
@@ -0,0 +1,14 @@
+namespace Comanda.WebApi.Entities;
+
+public sealed class Category : Entity
+{
+    public string Name { get; set; }
+
+    public Category()
+    {
+        /*
+            Default parameterless constructor included due to Entity Framework Core not setting navigation properties
+            when using constructors. For more information, see: https://learn.microsoft.com/pt-br/ef/core/modeling/constructors
+        */
+    }
+}
