fc0e18d469614c1c31ddf98f485814d11a87b5dd
https-richardy <code.richardy@gmail.com>
2024-06-24 19:54:24 -0300

    added "Name" property for the name of who is registering.

--- Source/Payloads/Requests/Account/AccountRegistrationRequest.cs
+++ Source/Payloads/Requests/Account/AccountRegistrationRequest.cs
@@ -2,6 +2,7 @@ namespace Comanda.WebApi.Payloads;
 
 public sealed record AccountRegistrationRequest : IRequest<Response>
 {
+    public string Name { get; init; }
     public string Email { get; init; }
     public string Password { get; init; }
     public EAccountType AccountType { get; init; }
