e7730e1ac2be0e61483ac5e7bd3e239c0a7385ed
https-richardy <code.richardy@gmail.com>
2024-06-24 14:18:21 -0300

    fix: changed interface type to "IEstablishmentOwnerRepository"

--- Source/Data/Repositories/EstablishmentOwnerRepository.cs
+++ Source/Data/Repositories/EstablishmentOwnerRepository.cs
@@ -2,7 +2,7 @@ namespace Comanda.WebApi.Data.Repositories;
 
 public sealed class EstablishmentOwnerRepository(ComandaDbContext dbContext) :
     Repository<EstablishmentOwner, ComandaDbContext>(dbContext),
-    IRepository<EstablishmentOwner>
+    IEstablishementOwnerRepository
 {
 
 }
