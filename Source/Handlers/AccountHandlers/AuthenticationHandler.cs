ffafde4d0e5c24c902b22f3197272901f6307063
https-richardy <code.richardy@gmail.com>
2024-06-24 17:56:49 -0300

    implemented AuthenticationHandler to handle user authentication requests.

---
+++ Source/Handlers/AccountHandlers/AuthenticationHandler.cs
@@ -0,0 +1,38 @@
+namespace Comanda.WebApi.Handlers;
+
+public sealed class AuthenticationHandler(
+    UserManager<Account> userManager,
+    IJwtService jwtService
+) : IRequestHandler<AuthenticationRequest, Response<AuthenticationResponse>>
+{
+    # pragma warning disable CS8604
+    public async Task<Response<AuthenticationResponse>> Handle(AuthenticationRequest request, CancellationToken cancellationToken)
+    {
+        var account = await userManager.FindByEmailAsync(request.Email);
+
+        if (account is null)
+            return Response<AuthenticationResponse>.Error(401, "invalid email or password.");
+
+        if (!await userManager.CheckPasswordAsync(account, request.Password))
+            return Response<AuthenticationResponse>.Error(401, "invalid email or password.");
+
+        var roles = await userManager.GetRolesAsync(account);
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.NameIdentifier, account.Id.ToString()),
+            new Claim(ClaimTypes.Name, account.UserName),
+            new Claim(ClaimTypes.Email, account.Email),
+        };
+
+        foreach (var role in roles)
+        {
+            claims.Add(new Claim(ClaimTypes.Role, role));
+        }
+
+        var claimsIdentity = new ClaimsIdentity(claims);
+        var token = jwtService.GenerateToken(claimsIdentity);
+
+        var response = new AuthenticationResponse { Token = token };
+        return new Response<AuthenticationResponse>(data: response, statusCode: 200 , message: "authentication successful.");
+    }
+}
