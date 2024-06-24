79d73dce5ae8fbcedc502afd458b06e2626ba2a5
https-richardy <code.richardy@gmail.com>
2024-06-24 14:35:36 -0300

    added nullable to allow FullName to be NULL

--- Source/Entities/Customer.cs
+++ Source/Entities/Customer.cs
@@ -2,7 +2,7 @@ namespace Comanda.WebApi.Entities;
 
 public sealed class Customer : Entity
 {
-    public string FullName { get; set; }
+    public string? FullName { get; set; }
 
     public Account Account { get; set; }
     public ICollection<Address> Addresses { get; set; } = [];
