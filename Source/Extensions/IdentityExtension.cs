3f6d634c879656de74d808798cf9e747dcb0e776
https-richardy <code.richardy@gmail.com>
2024-06-24 19:39:19 -0300

    enhanced ConfigureIdentity method in IdentityExtension to enforce unique email requirement and specify allowed username characters.

--- Source/Extensions/IdentityExtension.cs
+++ Source/Extensions/IdentityExtension.cs
@@ -4,7 +4,11 @@ public static class IdentityExtension
 {
     public static void ConfigureIdentity(this IServiceCollection services)
     {
-        services.AddIdentity<Account, IdentityRole>()
+        services.AddIdentity<Account, IdentityRole>(options =>
+        {
+            options.User.RequireUniqueEmail = true;
+            options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+        })
             .AddDefaultTokenProviders()
             .AddEntityFrameworkStores<ComandaDbContext>();
     }
