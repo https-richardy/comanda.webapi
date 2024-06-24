8efd8f818ec180b793c065a3b5888c7272c5071d
https-richardy <code.richardy@gmail.com>
2024-06-24 14:14:05 -0300

    added interface to define the contract of the establishment owners repository.

---
+++ Source/Data/Repositories/Interfaces/IEstablishmentOwnerRepository.cs
@@ -0,0 +1,6 @@
+namespace Comanda.WebApi.Data.Repositories;
+
+public interface IEstablishementOwnerRepository : IRepository<EstablishmentOwner>
+{
+    /* This interface inherits the contracts from IRepository. */
+}
