0bb1257edd2c26fb2d2ab8bbe6e6865187329fdf
https-richardy <code.richardy@gmail.com>
2024-06-24 11:44:07 -0300

    add MappingExtension for configuring mappings

---
+++ Source/Extensions/MappingExtension.cs
@@ -0,0 +1,17 @@
+namespace Comanda.WebApi.Extensions;
+
+public static class MappingExtension
+{
+    public static void AddMapping(this IServiceCollection services)
+    {
+        #region mappings for accounts requests
+
+        TinyMapper.Bind<AccountRegistrationRequest, Account>(config =>
+        {
+            config.Bind(source: source => source.Email, target: target => target.UserName);
+            config.Bind(source: source => source.Email, target: target => target.Email);
+        });
+
+        #endregion
+    }
+}
