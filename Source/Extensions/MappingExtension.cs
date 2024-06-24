d89c0c60e7604d647a64b3cd036cce815f10cf79
https-richardy <code.richardy@gmail.com>
2024-06-24 19:55:35 -0300

    added new property mapping from an account registration request Name to an account UserName

--- Source/Extensions/MappingExtension.cs
+++ Source/Extensions/MappingExtension.cs
@@ -8,7 +8,7 @@ public static class MappingExtension
 
         TinyMapper.Bind<AccountRegistrationRequest, Account>(config =>
         {
-            config.Bind(source: source => source.Email, target: target => target.UserName);
+            config.Bind(source: source => source.Name, target: target => target.UserName);
             config.Bind(source: source => source.Email, target: target => target.Email);
         });
 
