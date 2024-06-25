19784e84e469a3f54c94e9e09e4f81f66ea616f4
https-richardy <code.richardy@gmail.com>
2024-06-25 10:50:26 -0300

    add CreateEstablishmentProductRequest payload to handle establishment product creation requests

---
+++ Source/Payloads/Requests/Establishment/CreateEstablishmentProductRequest.cs
@@ -0,0 +1,12 @@
+namespace Comanda.WebApi.Payloads;
+
+public sealed record CreateEstablishmentProductRequest : IRequest<Response>, IAuthenticatedRequest
+{
+    public string Title { get; set; }
+    public string Description { get; init; }
+    public decimal Price { get; init; }
+    public IFormFile Image { get; init; }
+
+    [JsonIgnore]
+    public string UserId { get; set; } = string.Empty;
+}
