a4b7fe083ff7c1ec152b2baf43269a1d219dda84
https-richardy <code.richardy@gmail.com>
2024-06-25 00:18:29 -0300

    add CreateEstablishmentAsync action to EstablishmentController, utilizing Mediator.

--- Source/Controllers/EstablishmentController.cs
+++ Source/Controllers/EstablishmentController.cs
@@ -1,8 +1,18 @@
+#pragma warning disable CS8601
+
 namespace Comanda.WebApi.Controllers;
 
 [ApiController]
 [Route("api/establishments")]
-public sealed class EstablishmentController : ControllerBase
+public sealed class EstablishmentController(IMediator mediator) : ControllerBase
 {
+    [HttpPost]
+    [Authorize(Roles = "EstablishmentOwner")]
+    public async Task<IActionResult> CreateEstablishmentAsync(CreateEstablishmentRequest request)
+    {
+        request.UserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
+        var response = await mediator.Send(request);
+        return StatusCode(response.StatusCode, response);
+    }
 }
