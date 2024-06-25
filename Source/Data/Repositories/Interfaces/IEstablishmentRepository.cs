e370ace117868ba903de8f8d3dad94e69095cd99
https-richardy <code.richardy@gmail.com>
2024-06-24 21:40:33 -0300

    added "IEstablishmentRepository" interface inheriting contracts from IRepository<Establishment>

---
+++ Source/Data/Repositories/Interfaces/IEstablishmentRepository.cs
@@ -0,0 +1,6 @@
+namespace Comanda.WebApi.Data.Repositories;
+
+public interface IEstablishmentRepository : IRepository<Establishment>
+{
+    /* This interface inherits the contracts from IRepository. */
+}
