e0d545bc783afd1df0e765d3ac9b4b0bf7a4ebb2
https-richardy <code.richardy@gmail.com>
2024-06-24 21:11:03 -0300

    added CreateEstablishmentValidator to ensure validation rules for establishment creation requests

---
+++ Source/Validators/EstablishmentValidators/CreateEstablishmentValidator.cs
@@ -0,0 +1,15 @@
+namespace Comanda.WebApi.Validators;
+
+public sealed class CreateEstablishmentValidator :
+    AbstractValidator<CreateEstablishmentRequest>,
+    IValidator<CreateEstablishmentRequest>
+{
+    public CreateEstablishmentValidator()
+    {
+        RuleFor(request => request.EstablishmentName)
+            .NotEmpty()
+            .WithMessage("Establishment name is required.")
+            .MaximumLength(100)
+            .WithMessage("Establishment name must not exceed 100 characters.");
+    }
+}
