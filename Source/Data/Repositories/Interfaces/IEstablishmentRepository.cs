bcc238a4982b0bc11828c21f22cf7497659526b7
https-richardy <code.richardy@gmail.com>
2024-06-25 12:57:28 -0300

    added new contract in the IEstablishmentRepository interface to add a new product category in the establishment.

--- Source/Data/Repositories/Interfaces/IEstablishmentRepository.cs
+++ Source/Data/Repositories/Interfaces/IEstablishmentRepository.cs
@@ -3,4 +3,5 @@ namespace Comanda.WebApi.Data.Repositories;
 public interface IEstablishmentRepository : IRepository<Establishment>
 {
     Task AddProductAsync(Establishment establishment, Product product);
+    Task AddCategoryAsync(Establishment establishment, Category category);
 }
