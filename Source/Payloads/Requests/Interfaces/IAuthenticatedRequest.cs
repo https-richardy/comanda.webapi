25f1c275f2cfbd118c42c5d03c7886ef4c5491e5
https-richardy <code.richardy@gmail.com>
2024-06-24 21:02:27 -0300

    added IAuthenticatedRequest interface to ensure requests requiring user authentication contain a UserId.

---
+++ Source/Payloads/Requests/Interfaces/IAuthenticatedRequest.cs
@@ -0,0 +1,11 @@
+/// <summary>
+/// Interface to ensure that requests requiring user authentication contain a UserId.
+/// </summary>
+/// <remarks>
+/// Implementing this interface in request classes and records ensures that the UserId is included, 
+/// providing a contract that the request is made by an authenticated user.
+/// </remarks>
+public interface IAuthenticatedRequest
+{
+    string UserId { get; }
+}
