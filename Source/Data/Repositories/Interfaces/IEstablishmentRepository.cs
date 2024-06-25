6256e524ee5a721a6bd0a36d7a1b532d04332f46
https-richardy <code.richardy@gmail.com>
2024-06-25 14:09:51 -0300

    added contract to get the owner of the establishment

--- Source/Data/Repositories/Interfaces/IEstablishmentRepository.cs
+++ Source/Data/Repositories/Interfaces/IEstablishmentRepository.cs
@@ -4,4 +4,6 @@ public interface IEstablishmentRepository : IRepository<Establishment>
 {
     Task AddProductAsync(Establishment establishment, Product product);
     Task AddCategoryAsync(Establishment establishment, Category category);
+
+    Task<EstablishmentOwner> FindOwnerAsync(int establishmentId);
 }
