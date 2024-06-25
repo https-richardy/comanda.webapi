fada13b20961ec7d96856c092a48ce6c1235f80d
https-richardy <code.richardy@gmail.com>
2024-06-25 11:23:02 -0300

    add AddProductAsync method to EstablishmentRepository for adding products to establishments"

--- Source/Data/Repositories/EstablishmentRepository.cs
+++ Source/Data/Repositories/EstablishmentRepository.cs
@@ -1,8 +1,12 @@
 namespace Comanda.WebApi.Data.Repositories;
 
-public sealed class EstablishmentRepository(ComandaDbContext dbContext) : 
+public sealed class EstablishmentRepository(ComandaDbContext dbContext) :
     Repository<Establishment, ComandaDbContext>(dbContext),
     IEstablishmentRepository
 {
-    
+    public async Task AddProductAsync(Establishment establishment, Product product)
+    {
+        establishment.Products.Add(product);
+        await _dbContext.SaveChangesAsync();
+    }
 }
