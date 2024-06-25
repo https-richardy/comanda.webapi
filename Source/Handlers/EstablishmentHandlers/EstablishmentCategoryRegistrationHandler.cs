7270db24a13aa8a0348d796ff28a89faf245bac9
https-richardy <code.richardy@gmail.com>
2024-06-25 13:06:45 -0300

    add EstablishmentCategoryRegistrationHandler for handling establishment category registration

---
+++ Source/Handlers/EstablishmentHandlers/EstablishmentCategoryRegistrationHandler.cs
@@ -0,0 +1,30 @@
+namespace Comanda.WebApi.Handlers;
+
+public sealed class EstablishmentCategoryRegistrationHandler(
+    UserManager<Account> userManager,
+    IValidator<EstablishmentCategoryRegistrationRequest> validator,
+    IEstablishmentRepository establishmentRepository
+) : IRequestHandler<EstablishmentCategoryRegistrationRequest, Response>
+{
+    public async Task<Response> Handle(EstablishmentCategoryRegistrationRequest request,
+                                 CancellationToken cancellationToken)
+    {
+        var account = await userManager.FindByIdAsync(request.UserId);
+
+        var establishment = await establishmentRepository.FindSingleAsync(establishment => establishment.Id == request.EstablishmentId);
+        if (establishment is null)
+            return new Response(statusCode: 404, message: "establishment not found");
+
+        if (establishment.Owner.Account.Id != account?.Id)
+            return new Response(statusCode: 403, message: "The user is not the owner of this establishment.");
+
+        var validationResult = await validator.ValidateAsync(request);
+        if (!validationResult.IsValid)
+            throw new ValidationException(validationResult.Errors);
+
+        var category = TinyMapper.Map<Category>(request);
+        await establishmentRepository.AddCategoryAsync(establishment, category);
+
+        return new Response(statusCode: 201, message: "category created successfully.");
+    }
+}
