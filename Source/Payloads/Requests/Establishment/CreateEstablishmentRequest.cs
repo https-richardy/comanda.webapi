d4ea39b72995a823e69694effddee84aac1f2a88
https-richardy <code.richardy@gmail.com>
2024-06-24 22:34:20 -0300

    added "JsonIgnore" attribute to ignore serialization and deserialization of "UserId" property

--- Source/Payloads/Requests/Establishment/CreateEstablishmentRequest.cs
+++ Source/Payloads/Requests/Establishment/CreateEstablishmentRequest.cs
@@ -3,5 +3,7 @@ namespace Comanda.WebApi.Payloads;
 public record CreateEstablishmentRequest : IRequest<Response>, IAuthenticatedRequest
 {
     public string EstablishmentName { get; init; }
+
+    [JsonIgnore]
     public string UserId { get; set; } = string.Empty;
 }
