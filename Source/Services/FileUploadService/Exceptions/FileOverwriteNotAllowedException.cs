79cea6b71d95170baff3faa20da70b1c4c6f8f84
https-richardy <code.richardy@gmail.com>
2024-06-25 10:59:21 -0300

    added FileUploadService module

---
+++ Source/Services/FileUploadService/Exceptions/FileOverwriteNotAllowedException.cs
@@ -0,0 +1,12 @@
+namespace Comanda.WebApi.Services.Exceptions;
+
+/// <summary>
+/// Exception thrown when attempting to overwrite a file that already exists and overwriting is not allowed.
+/// </summary>
+public class FileOverwriteNotAllowedException : Exception
+{
+    public FileOverwriteNotAllowedException(string message) : base(message)
+    {
+
+    }
+}
---
+++ Source/Services/FileUploadService/Exceptions/InvalidFileExtensionException.cs
@@ -0,0 +1,12 @@
+namespace Comanda.WebApi.Services.Exceptions;
+
+/// <summary>
+/// Exception thrown when the file extension is not allowed.
+/// </summary>
+public class InvalidFileExtensionException : Exception
+{
+    public InvalidFileExtensionException(string message) : base(message)
+    {
+
+    }
+}
