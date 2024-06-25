2a4d00282bfd6a8fd799e6682eef242a55987b09
https-richardy <code.richardy@gmail.com>
2024-06-25 12:29:32 -0300

    added new migrations. Relationship between an establishment and a category has been added.

---
+++ Source/Data/Migrations/20240625152154_RelationshipBetweenACategoryAndAnEstablishment.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Comanda.WebApi.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RelationshipBetweenACategoryAndAnEstablishment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EstablishmentId",
+                table: "Categories",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_EstablishmentId",
+                table: "Categories",
+                column: "EstablishmentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Categories_Establishments_EstablishmentId",
+                table: "Categories",
+                column: "EstablishmentId",
+                principalTable: "Establishments",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Categories_Establishments_EstablishmentId",
+                table: "Categories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_EstablishmentId",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "EstablishmentId",
+                table: "Categories");
+        }
+    }
+}
