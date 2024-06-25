c821895d3aafa0f7d6cf09848e27e6560e2c52ef
https-richardy <code.richardy@gmail.com>
2024-06-25 14:41:33 -0300

    refactor CreateEstablishmentProductRequest to inherit from AuthenticatedRequest and include CategoryId

--- Source/Payloads/Requests/Establishment/CreateEstablishmentProductRequest.cs
+++ Source/Payloads/Requests/Establishment/CreateEstablishmentProductRequest.cs
@@ -1,14 +1,12 @@
 namespace Comanda.WebApi.Payloads;
 
-public sealed record CreateEstablishmentProductRequest : IRequest<Response>, IAuthenticatedRequest
+public sealed record CreateEstablishmentProductRequest : AuthenticatedRequest, IRequest<Response>
 {
     public string Title { get; set; }
     public string Description { get; init; }
     public decimal Price { get; init; }
+    public int CategoryId { get; init; }
     public IFormFile Image { get; init; }
-
-    [JsonIgnore]
-    public string UserId { get; set; } = string.Empty;
 
     [JsonIgnore]
     public int EstablishmentId { get; set; }
