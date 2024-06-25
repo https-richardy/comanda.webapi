c8f6322b35861d065cad3465ef76fafd747e7092
https-richardy <code.richardy@gmail.com>
2024-06-25 14:50:22 -0300

    added new contract to check if category exists

--- Source/Data/Repositories/Interfaces/IEstablishmentRepository.cs
+++ Source/Data/Repositories/Interfaces/IEstablishmentRepository.cs
@@ -4,6 +4,7 @@ public interface IEstablishmentRepository : IRepository<Establishment>
 {
     Task AddProductAsync(Establishment establishment, Product product);
     Task AddCategoryAsync(Establishment establishment, Category category);
+    Task<bool> CategoryExistsAsync(int categoryId);
 
     Task<EstablishmentOwner> FindOwnerAsync(int establishmentId);
 }
