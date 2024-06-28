ce70951545d4e1345034d144e9aa32a3124c17ea
https-richardy <code.richardy@gmail.com>
2024-06-28 11:58:40 -0300

    added endpoint to retrieve products from an establishment.

--- Source/Controllers/EstablishmentController.cs
+++ Source/Controllers/EstablishmentController.cs
@@ -6,6 +6,16 @@ namespace Comanda.WebApi.Controllers;
 [Route("api/establishments")]
 public sealed class EstablishmentController(IMediator mediator) : ControllerBase
 {
+    [HttpGet("{establishmentId}/products")]
+    public async Task<IActionResult> GetEstablishmentProductsAsync([FromQuery] GetEstablishmentProductsRequest request, int establishmentId)
+    {
+        request.EstablishmentId = establishmentId;
+
+        var response = await mediator.Send(request);
+        return StatusCode(response.StatusCode, response);
+    }
+
+
     [HttpPost]
     [Authorize(Roles = "EstablishmentOwner")]
     public async Task<IActionResult> CreateEstablishmentAsync(CreateEstablishmentRequest request)
