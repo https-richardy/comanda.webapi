5ff4a96f0f167de6330658bcfd15ac481e59fce8
https-richardy <code.richardy@gmail.com>
2024-06-28 11:59:15 -0300

    added contract to obtain products from a certain establishment.

--- Source/Data/Repositories/Interfaces/IEstablishmentRepository.cs
+++ Source/Data/Repositories/Interfaces/IEstablishmentRepository.cs
@@ -2,8 +2,10 @@ namespace Comanda.WebApi.Data.Repositories;
 
 public interface IEstablishmentRepository : IRepository<Establishment>
 {
+    Task<IEnumerable<Product>> GetProductsAsync(int pageNumber, int pageSize, int establishmentId);
     Task AddProductAsync(Establishment establishment, Product product);
     Task AddCategoryAsync(Establishment establishment, Category category);
+
     Task<Category> RetrieveCategoryByIdAsync(int categoryId);
     Task<bool> CategoryExistsAsync(int categoryId);
 
