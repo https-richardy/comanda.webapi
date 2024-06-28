b6cbcf28bb28d99a7a0607ef34cf96ca96495383
https-richardy <code.richardy@gmail.com>
2024-06-28 14:51:40 -0300

    added default values ​​for PageNumber and PageSize.

--- Source/Payloads/Requests/Establishment/GetEstablishmentProductsRequest.cs
+++ Source/Payloads/Requests/Establishment/GetEstablishmentProductsRequest.cs
@@ -2,8 +2,8 @@ namespace Comanda.WebApi.Payloads;
 
 public record GetEstablishmentProductsRequest : IRequest<Response<PaginationHelper<Product>>>
 {
-    public int PageNumber { get; init; }
-    public int PageSize { get; init; }
+    public int PageNumber { get; init; } = 1;
+    public int PageSize { get; init; } = 10;
 
     [JsonIgnore]
     public int EstablishmentId { get; set; }
