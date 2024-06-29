fbbb36381a1f612b7ad5ba4f3aa46be597cc8311
https-richardy <code.richardy@gmail.com>
2024-06-29 13:18:15 -0300

    added handler to handle requests to add a product to the cart

---
+++ Source/Handlers/CartHandlers/AddProductToCartHandler.cs
@@ -0,0 +1,70 @@
+namespace Comanda.WebApi.Handlers;
+
+public sealed class AddProductToCartHandler(
+    UserManager<Account> userManager,
+    CartManager cartManager,
+    ICustomerRepository customerRepository
+) : IRequestHandler<AddProductToCartRequest, Response>
+{
+    public async Task<Response> Handle(
+        AddProductToCartRequest request,
+        CancellationToken cancellationToken
+    )
+    {
+        var account = await userManager.FindByIdAsync(request.UserId);
+        if (account is null)
+            return new Response(
+                statusCode: StatusCodes.Status404NotFound,
+                message: "user not found."
+            );
+
+        if (!await userManager.IsInRoleAsync(account, "Customer"))
+            return new Response(
+                statusCode: StatusCodes.Status403Forbidden,
+                message: "user is not a customer. Only customers can add products to their cart."
+            );
+
+        var customer = await customerRepository.FindSingleAsync(customer => customer.Account.Id == account.Id);
+        if (customer is null)
+            return new Response(
+                statusCode: StatusCodes.Status404NotFound,
+                message: "customer not found."
+            );
+
+        try
+        {
+            await cartManager.AddItemAsync(
+                customerId: customer.Id,
+                productId: request.ProductId,
+                quantity: request.Quantity,
+                establishmentId: request.EstablishmentId
+            );
+
+            return new Response(
+                statusCode: StatusCodes.Status201Created,
+                message: "product added to cart successfully."
+            );
+        }
+        catch (CustomerNotFoundException exception)
+        {
+            return new Response(
+                statusCode: StatusCodes.Status404NotFound,
+                message: exception.Message
+            );
+        }
+        catch (EstablishmentNotFoundException exception)
+        {
+            return new Response(
+                statusCode: StatusCodes.Status404NotFound,
+                message: exception.Message
+            );
+        }
+        catch (EstablishmentProductNotFoundException exception)
+        {
+            return new Response(
+                statusCode: StatusCodes.Status404NotFound,
+                message: exception.Message
+            );
+        }
+    }
+}
