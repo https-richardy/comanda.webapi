6a4fccb83dd1416d2875934d8057906a7cb60014
https-richardy <code.richardy@gmail.com>
2024-06-29 15:23:55 -0300

    added endpoint to retrieve customer cart details in CartController

--- Source/Controllers/CartController.cs
+++ Source/Controllers/CartController.cs
@@ -6,6 +6,19 @@ namespace Comanda.WebApi.Controllers;
 [Route("api/carts")]
 public sealed class CartController(IMediator mediator) : ControllerBase
 {
+    [HttpGet]
+    [Authorize(Roles = "Customer")]
+    public async Task<IActionResult> GetCustomerCartAsync()
+    {
+        var request = new GetCartDetailsRequest
+        {
+            UserId = User.FindFirstValue(ClaimTypes.NameIdentifier)
+        };
+
+        var response = await mediator.Send(request);
+        return StatusCode(response.StatusCode, response);
+    }
+
     [HttpPost("add-item")]
     [Authorize(Roles = "Customer")]
     public async Task<IActionResult> AddItemToCustomerCartAsync(AddProductToCartRequest request)
