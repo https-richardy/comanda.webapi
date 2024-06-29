035aadb98d19b8511f98c0bde6d65d3819c66079
https-richardy <code.richardy@gmail.com>
2024-06-29 00:58:00 -0300

    added DbSet's for Cart and CartItem.

--- Source/Data/ComandaDbContext.cs
+++ Source/Data/ComandaDbContext.cs
@@ -8,6 +8,8 @@ public sealed class ComandaDbContext(DbContextOptions options) : IdentityDbConte
     public DbSet<Product> Products { get; set; }
     public DbSet<Order> Orders { get; set; }
     public DbSet<OrderItem> OrderItems { get; set; }
+    public DbSet<Cart> Carts { get; set; }
+    public DbSet<CartItem> CartItems { get; set; }
     public DbSet<Customer> Customers { get; set; }
     public DbSet<Address> Addresses { get; set; }
 }
