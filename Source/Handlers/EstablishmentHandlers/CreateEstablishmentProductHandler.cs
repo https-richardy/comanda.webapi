9d0804231d97306237db551ccb5513871d1f5e14
https-richardy <code.richardy@gmail.com>
2024-06-29 15:04:59 -0300

    style: formatted document.

--- Source/Handlers/EstablishmentHandlers/CreateEstablishmentProductHandler.cs
+++ Source/Handlers/EstablishmentHandlers/CreateEstablishmentProductHandler.cs
@@ -9,36 +9,36 @@ public sealed class CreateEstablishmentProductHandler(
 {
     public async Task<Response> Handle(CreateEstablishmentProductRequest request, CancellationToken cancellationToken)
     {
-            var account = await userManager.FindByIdAsync(request.UserId);
-            if (account is null)
-                return new Response(statusCode: 404, message: "user not found");
+        var account = await userManager.FindByIdAsync(request.UserId);
+        if (account is null)
+            return new Response(statusCode: 404, message: "user not found");
 
-            var establishment = await establishmentRepository.RetrieveByIdAsync(request.EstablishmentId);
-            if (establishment is null)
-                return new Response(statusCode: 404, message: "establishment not found");
+        var establishment = await establishmentRepository.RetrieveByIdAsync(request.EstablishmentId);
+        if (establishment is null)
+            return new Response(statusCode: 404, message: "establishment not found");
 
-            var owner = await establishmentRepository.FindOwnerAsync(establishment.Id);
-            if (owner.Account.Id != account.Id)
-                return new Response(statusCode: 403, message: "The user is not the owner of this establishment.");
+        var owner = await establishmentRepository.FindOwnerAsync(establishment.Id);
+        if (owner.Account.Id != account.Id)
+            return new Response(statusCode: 403, message: "The user is not the owner of this establishment.");
 
-            if (!await CanCreateProductAsync(account))
-                return new Response(statusCode: 403, message: "the user does not have an establishment. The user must have a registered establishment before registering a product.");
+        if (!await CanCreateProductAsync(account))
+            return new Response(statusCode: 403, message: "the user does not have an establishment. The user must have a registered establishment before registering a product.");
 
-            if (!await establishmentRepository.CategoryExistsAsync(request.CategoryId))
-                return new Response(statusCode: 404, message: "this category does not exist.");
+        if (!await establishmentRepository.CategoryExistsAsync(request.CategoryId))
+            return new Response(statusCode: 404, message: "this category does not exist.");
 
-            var validationResult = await validator.ValidateAsync(request);
-            if (!validationResult.IsValid)
-                throw new ValidationException(validationResult.Errors);
+        var validationResult = await validator.ValidateAsync(request);
+        if (!validationResult.IsValid)
+            throw new ValidationException(validationResult.Errors);
 
-            var product = TinyMapper.Map<Product>(request);
+        var product = TinyMapper.Map<Product>(request);
 
-            product.Category = await establishmentRepository.RetrieveCategoryByIdAsync(request.CategoryId);
-            product.ImagePath = await fileUploadService.UploadFileAsync(request.Image);
+        product.Category = await establishmentRepository.RetrieveCategoryByIdAsync(request.CategoryId);
+        product.ImagePath = await fileUploadService.UploadFileAsync(request.Image);
 
-            await establishmentRepository.AddProductAsync(establishment, product);
+        await establishmentRepository.AddProductAsync(establishment, product);
 
-            return new Response(statusCode: 201, message: "product created successfully.");
+        return new Response(statusCode: 201, message: "product created successfully.");
     }
 
     private async Task<bool> CanCreateProductAsync(Account account)
