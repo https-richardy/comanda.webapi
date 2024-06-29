e1734d7e930f26dfe05fe700903d32740c55fb56
https-richardy <code.richardy@gmail.com>
2024-06-29 11:44:21 -0300

    added new contract to recover a single product from a specific establishment.

--- Source/Data/Repositories/Interfaces/IEstablishmentRepository.cs
+++ Source/Data/Repositories/Interfaces/IEstablishmentRepository.cs
@@ -2,6 +2,7 @@ namespace Comanda.WebApi.Data.Repositories;
 
 public interface IEstablishmentRepository : IRepository<Establishment>
 {
+    Task<Product?> GetProductByIdAsync(int productId, int establishmentId);
     Task<IEnumerable<Product>> GetProductsAsync(int pageNumber, int pageSize, int establishmentId);
     Task AddProductAsync(Establishment establishment, Product product);
     Task AddCategoryAsync(Establishment establishment, Category category);
