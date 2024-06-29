e7ef582946fc8b597339e584a17b9fbfc8407ad0
https-richardy <code.richardy@gmail.com>
2024-06-29 11:16:12 -0300

    added ICartRepository interface to define contracts for cart repository.

---
+++ Source/Data/Repositories/Interfaces/ICartRepository.cs
@@ -0,0 +1,9 @@
+namespace Comanda.WebApi.Data.Repositories;
+
+public interface ICartRepository : IMinimalRepository<Cart>
+{
+    Task AddItemAsync(Cart cart, CartItem item);
+    Task RemoveItemAsync(Cart cart, CartItem item);
+    Task ClearCartAsync(Cart cart);
+    Task<Cart?> FindCartByCustomerIdAsync(int customerId);
+}
