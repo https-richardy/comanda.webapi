af2adee86393719215fce018c8395b58c82d2a40
https-richardy <code.richardy@gmail.com>
2024-06-29 00:56:33 -0300

    added cart entity to a temporary shopping state, representing a temporary selection of products the customer is considering.

---
+++ Source/Entities/Cart.cs
@@ -0,0 +1,39 @@
+namespace Comanda.WebApi.Entities;
+
+public sealed class Cart : Entity
+{
+    public decimal Total => Items.Sum(item => item.Product.Price * item.Quantity);
+
+    public Customer Customer { get; set; }
+    public ICollection<CartItem> Items { get; set; } = [];
+
+    public Cart()
+    {
+        /*
+            Default parameterless constructor included due to Entity Framework Core not setting navigation properties
+            when using constructors. For more information, see: https://learn.microsoft.com/pt-br/ef/core/modeling/constructors
+        */
+    }
+
+    public Cart(Customer customer)
+    {
+        Customer = customer;
+    }
+
+    public void AddItem(Product product, int quantity)
+    {
+        var existingItem = Items.FirstOrDefault(item => item.Product.Id == product.Id);
+
+        if (existingItem != null)
+            existingItem.Quantity += quantity;
+        else
+            Items.Add(new CartItem(quantity, product));
+    }
+
+    public void RemoveItem(Product product)
+    {
+        var item = Items.FirstOrDefault(item => item.Product.Id == product.Id);
+        if (item != null)
+            Items.Remove(item);
+    }
+}
