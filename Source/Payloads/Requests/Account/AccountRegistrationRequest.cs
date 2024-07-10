ac86b4d33b6c9e27fe3b432505886a8392b3b474
https-richardy <code.richardy@gmail.com>
2024-07-10 18:08:53 -0300

    removed account type enumerator.

--- Source/Payloads/Requests/Account/AccountRegistrationRequest.cs
+++ Source/Payloads/Requests/Account/AccountRegistrationRequest.cs
@@ -5,5 +5,4 @@ public sealed record AccountRegistrationRequest : IRequest<Response>
     public string Name { get; init; }
     public string Email { get; init; }
     public string Password { get; init; }
-    public EAccountType AccountType { get; init; }
 }
