0413b7e92e91481ab210f9f043c799916ce67c8d
https-richardy <code.richardy@gmail.com>
2024-07-10 19:06:40 -0300

    added new validation rule for Name property.

--- Source/Validators/IdentityValidators/AccountRegistrationValidator.cs
+++ Source/Validators/IdentityValidators/AccountRegistrationValidator.cs
@@ -6,6 +6,10 @@ public sealed class AccountRegistrationValidator :
 {
     public AccountRegistrationValidator()
     {
+        RuleFor(request => request.Name)
+            .NotEmpty().WithMessage("Full name is required.")
+            .MinimumLength(3).WithMessage("Full name must be at least 3 characters.");
+
         RuleFor(request => request.Email)
             .NotEmpty().WithMessage("Email is required.")
             .EmailAddress().WithMessage("Invalid email address format.");
