79647dc6ed34913a8054f4d3dc7240faeac815d7
https-richardy <code.richardy@gmail.com>
2024-07-15 19:51:03 -0300

    improved Authentication Handler with dedicated service

--- Source/Handlers/IdentityHandlers/AuthenticationHandler.cs
+++ Source/Handlers/IdentityHandlers/AuthenticationHandler.cs
@@ -4,46 +4,37 @@ public sealed class AuthenticationHandler :
     IRequestHandler<AuthenticationCredentials, Response<AuthenticationResponse>>
 {
     private readonly UserManager<Account> _userManager;
+    private readonly IAuthenticationService _authenticationService;
     private readonly IJwtService _jwtService;
 
-    public AuthenticationHandler(UserManager<Account> userManager, IJwtService jwtService)
+    public AuthenticationHandler(
+        UserManager<Account> userManager,
+        IAuthenticationService authenticationService,
+        IJwtService jwtService
+    )
     {
         _userManager = userManager;
         _jwtService = jwtService;
+        _authenticationService = authenticationService;
     }
 
     # pragma warning disable CS8604
-    public async Task<Response<AuthenticationResponse>> Handle(AuthenticationCredentials request, CancellationToken cancellationToken)
+    public async Task<Response<AuthenticationResponse>> Handle(
+        AuthenticationCredentials request,
+        CancellationToken cancellationToken
+    )
     {
+        var validCredentials = await _authenticationService.ValidateCredentialsAsync(request);
+        if (validCredentials is false)
+            return new Response<AuthenticationResponse>(
+                data: null,
+                statusCode: StatusCodes.Status401Unauthorized,
+                message: "invalid credentials."
+            );
+
         var account = await _userManager.FindByEmailAsync(request.Email);
-        if (account is null)
-            return new Response<AuthenticationResponse>(
-                data: null,
-                statusCode: StatusCodes.Status401Unauthorized,
-                message: "invalid email or password."
-            );
+        var claimsIdentity = await _authenticationService.BuildClaimsIdentity(account);
 
-        if (!await _userManager.CheckPasswordAsync(account, request.Password))
-            return new Response<AuthenticationResponse>(
-                data: null,
-                statusCode: StatusCodes.Status401Unauthorized,
-                message: "invalid email or password."
-            );
-
-        var roles = await _userManager.GetRolesAsync(account);
-        var claims = new List<Claim>
-        {
-            new Claim(ClaimTypes.NameIdentifier, account.Id.ToString()),
-            new Claim(ClaimTypes.Name, account.UserName),
-            new Claim(ClaimTypes.Email, account.Email),
-        };
-
-        foreach (var role in roles)
-        {
-            claims.Add(new Claim(ClaimTypes.Role, role));
-        }
-
-        var claimsIdentity = new ClaimsIdentity(claims);
         var token = _jwtService.GenerateToken(claimsIdentity);
 
         var response = new AuthenticationResponse { Token = token };
