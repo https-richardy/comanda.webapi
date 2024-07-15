3c0d15496f67d5042c07af126c324ef193c514f1
https-richardy <code.richardy@gmail.com>
2024-07-15 20:45:22 -0300

    style: remove blank line.

--- Source/Payloads/Requests/CartPayloads/InsertProductIntoCartRequest.cs
+++ Source/Payloads/Requests/CartPayloads/InsertProductIntoCartRequest.cs
@@ -1,6 +1,6 @@
 namespace Comanda.WebApi.Payloads;
 
-public sealed record InsertProductIntoCartRequest : 
+public sealed record InsertProductIntoCartRequest :
     AuthenticatedRequest, IRequest<Response>
 {
     public int ProductId { get; init; }
