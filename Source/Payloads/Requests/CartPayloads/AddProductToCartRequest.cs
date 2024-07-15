1cb6b7db536d2869aab7c6c3ce330587c1041951
https-richardy <code.richardy@gmail.com>
2024-07-15 20:45:00 -0300

    file and record renamed.

--- Source/Payloads/Requests/CartPayloads/AddProductToCartRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace Comanda.WebApi.Payloads;
-
-public sealed record AddProductToCartRequest : AuthenticatedRequest, IRequest<Response>
-{
-    public int ProductId { get; init; }
-    public int Quantity { get; init; }
-    public int EstablishmentId { get; init; }
-}
---
+++ Source/Payloads/Requests/CartPayloads/InsertProductIntoCartRequest.cs
@@ -0,0 +1,8 @@
+namespace Comanda.WebApi.Payloads;
+
+public sealed record InsertProductIntoCartRequest : 
+    AuthenticatedRequest, IRequest<Response>
+{
+    public int ProductId { get; init; }
+    public int Quantity { get; init; }
+}
