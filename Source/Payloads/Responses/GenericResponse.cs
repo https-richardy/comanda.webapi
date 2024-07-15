93029e3aae7f1f7187cf32c6bdd7655ec26ad447
https-richardy <code.richardy@gmail.com>
2024-07-15 14:38:30 -0300

    response that accepted a generic type was separated into a single a

---
+++ Source/Payloads/Responses/GenericResponse.cs
@@ -0,0 +1,22 @@
+namespace Comanda.WebApi.Payloads;
+
+public class Response<TData>
+{
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public TData? Data { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? Message { get; set; }
+
+    public int StatusCode { get; set; } = 200;
+    public bool IsSuccess => StatusCode >= 200 && StatusCode < 300;
+
+    public Response() { }
+
+    public Response(TData? data, int statusCode, string? message)
+    {
+        Data = data;
+        StatusCode = statusCode;
+        Message = message;
+    }
+}
