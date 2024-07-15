894ff1c33c60c977ac43856dd1b59371b025269a
https-richardy <code.richardy@gmail.com>
2024-07-15 15:13:38 -0300

    Improvement: resolved ambiguous references and modified Error typing to only accept errors directly: IEnumerable<ValidationFailure>

--- Source/Payloads/Responses/ValidationFailureResponse.cs
+++ Source/Payloads/Responses/ValidationFailureResponse.cs
@@ -1,11 +1,18 @@
+/* 
+    using aliases because of the ambiguous reference between 
+    FluentValidation.Results.ValidationFailure and Microsoft.IdentityModel.Tokens.ValidationFailure
+*/
+
+using ValidationFailure = FluentValidation.Results.ValidationFailure;
+
 namespace Comanda.WebApi.Payloads;
 
 public sealed class ValidationFailureResponse : Response
 {
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-    public ValidationResult Errors { get; set; }
+    public IEnumerable<ValidationFailure> Errors { get; set; }
 
-    public ValidationFailureResponse(ValidationResult errors)
+    public ValidationFailureResponse(IEnumerable<ValidationFailure> errors)
     {
         StatusCode = StatusCodes.Status400BadRequest;
         Message = "We couldn't process your request because some fields are missing or invalid. Please review the details and try again.";
