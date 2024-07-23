b46d7c1b658fea849ba4e92a5047ba9679409406
https-richardy <code.richardy@gmail.com>
2024-07-23 12:57:21 -0300

    added attribute to ignore the CategoryId property in the Json deserialization, as it will be set coming from the route.

--- Source/Payloads/Requests/CategoryPayloads/CategoryEditingRequest.cs
+++ Source/Payloads/Requests/CategoryPayloads/CategoryEditingRequest.cs
@@ -3,5 +3,7 @@ namespace Comanda.WebApi.Payloads;
 public sealed record CategoryEditingRequest : IRequest<Response>
 {
     public string Title { get; init; }
+
+    [JsonIgnore]
     public int CategoryId { get; set; }
 }
