63fd88fb86407f9f22d3a0dfb224f6283009f4f1
https-richardy <code.richardy@gmail.com>
2024-07-24 12:17:33 -0300

    added Response<TData> to take a payload

--- Source/Payloads/Requests/CategoryPayloads/CategoryDetailRequest.cs
+++ Source/Payloads/Requests/CategoryPayloads/CategoryDetailRequest.cs
@@ -1,6 +1,6 @@
 namespace Comanda.WebApi.Payloads;
 
-public sealed record CategoryDetailRequest : IRequest<Response>
+public sealed record CategoryDetailRequest : IRequest<Response<Category>>
 {
     [JsonIgnore]
     public int CategoryId { get; set; }
