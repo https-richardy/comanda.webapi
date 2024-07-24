1054bf3ea3b7f736fac248314bb63b04a191716f
https-richardy <code.richardy@gmail.com>
2024-07-24 14:49:06 -0300

    repository for accessing and persisting product data has been registered.

--- Source/Extensions/DataPersistenceExtension.cs
+++ Source/Extensions/DataPersistenceExtension.cs
@@ -11,6 +11,7 @@ public static class DataPersistenceExtension
         });
 
         services.AddScoped<ICategoryRepository, CategoryRepository>();
+        services.AddScoped<IProductRepository, ProductRepository>();
         services.AddScoped<ICustomerRepository, CustomerRepository>();
         services.AddScoped<ICartRepository, CartRepository>();
     }
