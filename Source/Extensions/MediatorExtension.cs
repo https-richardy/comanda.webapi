f6a05392ee8eb5f89a1aabc90bd08299c07c3708
https-richardy <code.richardy@gmail.com>
2024-07-24 12:29:40 -0300

    registered batch category-related request handlers.

--- Source/Extensions/MediatorExtension.cs
+++ Source/Extensions/MediatorExtension.cs
@@ -15,5 +15,15 @@ public static class MediatorExtension
         services.AddScoped<IRequestHandler<AuthenticationCredentials, Response<AuthenticationResponse>>, AuthenticationHandler>();
 
         #endregion
+
+        #region handlers for category requests
+
+        services.AddScoped<IRequestHandler<CategoryDetailRequest, Response<Category>>, CategoryDetailHandler>();
+        services.AddScoped<IRequestHandler<CategoryListingRequest, Response<IEnumerable<Category>>>, CategoryListingHandler>();
+        services.AddScoped<IRequestHandler<CategoryCreationRequest, Response>, CategoryCreationHandler>();
+        services.AddScoped<IRequestHandler<CategoryEditingRequest, Response>, CategoryEditingHandler>();
+        services.AddScoped<IRequestHandler<CategoryDeletionRequest, Response>, CategoryDeletionHandler>();
+
+        #endregion
     }
 }
