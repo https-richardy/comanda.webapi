183a0122ebd9edfbf8db118bc5c4d2a36840edf0
https-richardy <code.richardy@gmail.com>
2024-07-25 10:39:51 -0300

    added record representing the payload of a request to update/edit a product.

---
+++ Source/Payloads/Requests/ProductPayloads/ProductEditingRequest.cs
@@ -0,0 +1,13 @@
+namespace Comanda.WebApi.Payloads;
+
+public sealed record ProductEditingRequest : IRequest<Response>
+{
+    [JsonIgnore]
+    public int ProductId { get; set; }
+
+    public string Title { get; init; }
+    public string Description { get; init; }
+    public decimal Price { get; init; }
+    public int CategoryId { get; init; }
+    public IFormFile Image { get; init; }
+}
