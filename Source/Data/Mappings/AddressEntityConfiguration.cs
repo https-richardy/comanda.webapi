da11bc613082c80b522556ea02bda935be14ea80
https-richardy <code.richardy@gmail.com>
2024-07-25 12:54:20 -0300

    added settings for mapping the 'Address' entity to the database using FluentAPI.

---
+++ Source/Data/Mappings/AddressEntityConfiguration.cs
@@ -0,0 +1,41 @@
+namespace Comanda.WebApi.Data.Mappings;
+
+public sealed class AddressEntityConfiguration : IEntityTypeConfiguration<Address>
+{
+    private const string _tableName = "Addresses";
+
+    public void Configure(EntityTypeBuilder<Address> builder)
+    {
+        builder.ToTable(_tableName);
+        builder.HasKey(address => address.Id);
+
+        builder.Property(address => address.Street)
+            .IsRequired()
+            .HasMaxLength(160);
+
+        builder.Property(address => address.Number)
+            .HasDefaultValue("S/N")
+            .HasMaxLength(8);
+
+        builder.Property(address => address.City)
+            .IsRequired()
+            .HasMaxLength(160);
+
+        builder.Property(address => address.State)
+            .IsRequired()
+            .HasMaxLength(2);
+
+        builder.Property(address => address.Neighborhood)
+            .IsRequired();
+
+        builder.Property(address => address.PostalCode)
+            .HasMaxLength(8)
+            .IsRequired();
+
+        builder.Property(address => address.Complement)
+            .HasMaxLength(160);
+
+        builder.Property(address => address.Reference)
+            .HasMaxLength(160);
+    }
+}
