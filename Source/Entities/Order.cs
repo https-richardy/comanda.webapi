1648d438f9bb9390d253fc8a90ba316f856f6404
https-richardy <code.richardy@gmail.com>
2024-07-25 13:13:19 -0300

    added method for calculating the total price reflecting the new OrderItem structure.

--- Source/Entities/Order.cs
+++ Source/Entities/Order.cs
@@ -2,8 +2,9 @@ namespace Comanda.WebApi.Entities;
 
 public sealed class Order : Entity
 {
-    public decimal Total => OrderItems.Sum(item => item.Price);
+    public decimal Total => CalculateTotal();
     public Customer Customer { get; set; }
+    public Address ShippingAddress { get; set; }
 
     public ICollection<OrderItem> OrderItems { get; set; } = [];
     public EOrderStatus Status { get; set; }
@@ -15,5 +16,10 @@ public sealed class Order : Entity
             Default parameterless constructor included due to Entity Framework Core not setting navigation properties
             when using constructors. For more information, see: https://learn.microsoft.com/pt-br/ef/core/modeling/constructors
         */
+    }
+
+    private decimal CalculateTotal()
+    {
+        return OrderItems.Sum(item => item.Product.Price * item.Quantity);
     }
 }
