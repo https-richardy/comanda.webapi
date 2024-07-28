6188a7f8bf1e21155e81924ffb588d9160551b45
https-richardy <code.richardy@gmail.com>
2024-07-27 23:32:50 -0300

    New contract added: contract for recovering a customer by user/account ID.

--- Source/Data/Repositories/Interfaces/ICustomerRepository.cs
+++ Source/Data/Repositories/Interfaces/ICustomerRepository.cs
@@ -2,5 +2,5 @@ namespace Comanda.WebApi.Data.Repositories;
 
 public interface ICustomerRepository : IRepository<Customer>
 {
-    /* This interface inherits the contracts from IRepository. */
+    Task<Customer?> FindCustomerByUserIdAsync(string userId);
 }
