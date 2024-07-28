c81bf3865fccdf32832db4b1921e70e353f3e02f
https-richardy <code.richardy@gmail.com>
2024-07-27 23:30:12 -0300

    added request handler for inserting a product in the cart.

---
+++ Source/Handlers/CartHandlers/InsertProductToCartHandler.cs
@@ -0,0 +1,47 @@
+namespace Comanda.WebApi.Handlers;
+
+public sealed class InsertProductToCartHandler(
+    ICartRepository cartRepository,
+    IProductRepository productRepository,
+    ICustomerRepository customerRepository,
+    IUserContextService userContextService
+) : IRequestHandler<InsertProductIntoCartRequest, Response>
+{
+    public async Task<Response> Handle(
+        InsertProductIntoCartRequest request,
+        CancellationToken cancellationToken
+    )
+    {
+        var userIdentifier = userContextService.GetCurrentUserIdentifier();
+
+        var product = await productRepository.RetrieveByIdAsync(request.ProductId);
+        if (product is null)
+            return new Response(
+                statusCode: StatusCodes.Status404NotFound,
+                message: "Product not found."
+            );
+
+        /*
+            It is impossible for the 'userIdentifier' to be null at this point
+            since the 'CartController' restricts access only to authenticated customers.
+        */
+        #pragma warning disable CS8604
+
+        var customer = await customerRepository.RetrieveByIdAsync(userIdentifier);
+        var cart = await cartRepository.FindCartByCustomerIdAsync(customer.Id);
+
+        if (cart is null)
+        {
+            cart = new Cart { Customer = customer };
+            await cartRepository.SaveAsync(cart);
+        }
+
+        cart.AddItem(product, request.Quantity);
+        await cartRepository.UpdateAsync(cart);
+
+        return new Response(
+            statusCode: StatusCodes.Status201Created,
+            message: "Product added to cart successfully."
+        );
+    }
+}
