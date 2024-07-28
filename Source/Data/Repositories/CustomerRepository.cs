eb0db2366c408692bb299e15e9cb7b36a4c0e7c8
https-richardy <code.richardy@gmail.com>
2024-07-27 23:36:43 -0300

    implemented the new contract for recovering a customer by account/user ID.

--- Source/Data/Repositories/CustomerRepository.cs
+++ Source/Data/Repositories/CustomerRepository.cs
@@ -4,5 +4,10 @@ public sealed class CustomerRepository(ComandaDbContext dbContext) :
     Repository<Customer, ComandaDbContext>(dbContext),
     ICustomerRepository
 {
-
+    public async Task<Customer?> FindCustomerByUserIdAsync(string userId)
+    {
+        return await _dbContext.Customers
+            .Where(customer => customer.Account.Id == userId)
+            .FirstOrDefaultAsync();
+    }
 }
