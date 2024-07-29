ec0f7c52f93801ee7527585a1ed267d91961e33c
https-richardy <code.richardy@gmail.com>
2024-07-29 11:52:51 -0300

    added new entity: Ingredients.

---
+++ Source/Entities/Ingredient.cs
@@ -0,0 +1,21 @@
+namespace Comanda.WebApi.Entities;
+
+public sealed class Ingredient : Entity
+{
+    public string Name { get; set; }
+    public int StandardQuantity { get; set; }
+
+    public Ingredient()
+    {
+        /*
+            Default parameterless constructor included due to Entity Framework Core not setting navigation properties
+            when using constructors. For more information, see: https://learn.microsoft.com/pt-br/ef/core/modeling/constructors
+        */
+    }
+
+    public Ingredient(string name, int standardQuantity)
+    {
+        Name = name;
+        StandardQuantity = standardQuantity;
+    }
+}
