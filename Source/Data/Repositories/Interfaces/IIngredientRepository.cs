d3c3be1a015f699acad60df0494940d23f851b93
https-richardy <code.richardy@gmail.com>
2024-07-29 12:17:03 -0300

    added interface defining contracts for access and persistence of data related to ingredients.

---
+++ Source/Data/Repositories/Interfaces/IIngredientRepository.cs
@@ -0,0 +1,6 @@
+namespace Comanda.WebApi.Data.Repositories;
+
+public interface IIngredientRepository : IMinimalRepository<Ingredient>
+{
+    /* This interface inherits the contracts from IMinimalRepository. */
+}
