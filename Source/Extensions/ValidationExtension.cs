bd2b1d589e9e94d159fd18e0a275bd0c1b71a80f
https-richardy <code.richardy@gmail.com>
2024-07-30 10:59:20 -0300

    added a new region for registering ingredient-related request validators and also added a request validator for creating a new ingredient.

--- Source/Extensions/ValidationExtension.cs
+++ Source/Extensions/ValidationExtension.cs
@@ -19,6 +19,12 @@ public static class ValidationExtension
 
         #endregion
 
+        #region validators for ingredient requests
+
+        services.AddScoped<IValidator<IngredientCreationRequest>, IngredientCreationValidator>();
+
+        #endregion
+
         #region validators for cart requests
 
         services.AddScoped<IValidator<UpdateItemQuantityInCartRequest>, UpdateItemQuantityValidator>();
