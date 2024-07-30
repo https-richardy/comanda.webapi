81c0f116dbd84eea4c2b4c571df31807f7011beb
https-richardy <code.richardy@gmail.com>
2024-07-30 11:05:51 -0300

    added new data migrations: with the addition of the new entities 'Additional', 'Ingredient', 'ProductIngredient' it was necessary to change the structure of the database to support the customization of the order.

---
+++ Source/Data/Migrations/20240730140427_NewSchemesAdditionalsIngredients.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Comanda.WebApi.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewSchemesAdditionalsIngredients : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Additionals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Additionals", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Additionals_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Additionals_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ingredients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ingredients", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CartItemAdditional",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    AdditionalId = table.Column<int>(type: "int", nullable: false),
+                    CartItemId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CartItemAdditional", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CartItemAdditional_Additionals_AdditionalId",
+                        column: x => x.AdditionalId,
+                        principalTable: "Additionals",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CartItemAdditional_CartItems_CartItemId",
+                        column: x => x.CartItemId,
+                        principalTable: "CartItems",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductIngredients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StandardQuantity = table.Column<int>(type: "int", nullable: false),
+                    IsMandatory = table.Column<bool>(type: "bit", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    IngredientId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductIngredients", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProductIngredients_Ingredients_IngredientId",
+                        column: x => x.IngredientId,
+                        principalTable: "Ingredients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductIngredients_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Additionals_CategoryId",
+                table: "Additionals",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Additionals_ProductId",
+                table: "Additionals",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartItemAdditional_AdditionalId",
+                table: "CartItemAdditional",
+                column: "AdditionalId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartItemAdditional_CartItemId",
+                table: "CartItemAdditional",
+                column: "CartItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductIngredients_IngredientId",
+                table: "ProductIngredients",
+                column: "IngredientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductIngredients_ProductId",
+                table: "ProductIngredients",
+                column: "ProductId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CartItemAdditional");
+
+            migrationBuilder.DropTable(
+                name: "ProductIngredients");
+
+            migrationBuilder.DropTable(
+                name: "Additionals");
+
+            migrationBuilder.DropTable(
+                name: "Ingredients");
+        }
+    }
+}
