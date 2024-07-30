c5759625bfa2ef1498bad1b13fffc36bbf5ed0dc
https-richardy <code.richardy@gmail.com>
2024-07-30 00:21:09 -0300

    added new entity: ProductIngredient

---
+++ Source/Entities/ProductIngredient.cs
@@ -0,0 +1,24 @@
+namespace Comanda.WebApi.Entities;
+
+public sealed class ProductIngredient : Entity
+{
+    public int StandardQuantity { get; set; }
+
+    public Product Product { get; set; }
+    public Ingredient Ingredient { get; set; }
+
+    public ProductIngredient()
+    {
+        /*
+            Default parameterless constructor included due to Entity Framework Core not setting navigation properties
+            when using constructors. For more information, see: https://learn.microsoft.com/pt-br/ef/core/modeling/constructors
+        */
+    }
+
+    public ProductIngredient(Product product, Ingredient ingredient, int standardQuantity)
+    {
+        Product = product;
+        Ingredient = ingredient;
+        StandardQuantity = standardQuantity;
+    }
+}
