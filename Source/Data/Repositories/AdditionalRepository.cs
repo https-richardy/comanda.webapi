5e805bbc3bd9671ac48bbee61b24e23e76f36099
https-richardy <code.richardy@gmail.com>
2024-07-30 15:29:39 -0300

    updated: overridden the retrieval method of all database objects to include the navigation property representing the category.

--- Source/Data/Repositories/AdditionalRepository.cs
+++ Source/Data/Repositories/AdditionalRepository.cs
@@ -1,8 +1,14 @@
+
 namespace Comanda.WebApi.Data.Repositories;
 
 public sealed class AdditionalRepository(ComandaDbContext dbContext) :
     MinimalRepository<Additional, ComandaDbContext>(dbContext),
     IAdditionalRepository
 {
-
+    public async override Task<IEnumerable<Additional>> RetrieveAllAsync()
+    {
+        return await _dbContext.Additionals
+            .Include(cart => cart.Category)
+            .ToListAsync();
+    }
 }
