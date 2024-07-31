fa46f6deabecfcdafcc97e501b950c7abffab5a5
https-richardy <code.richardy@gmail.com>
2024-07-31 10:55:07 -0300

    handler updated to reflect the latest changes to the ProductCreationRequest which now returns a ProductCreationResponse.

--- Source/Handlers/ProductHandlers/ProductCreationHandler.cs
+++ Source/Handlers/ProductHandlers/ProductCreationHandler.cs
@@ -6,20 +6,21 @@ public sealed class ProductCreationHandler(
     ICategoryRepository categoryRepository,
     IValidator<ProductCreationRequest> validator,
     ILogger<ProductCreationHandler> logger
-) : IRequestHandler<ProductCreationRequest, Response>
+) : IRequestHandler<ProductCreationRequest, Response<ProductCreationResponse>>
 {
-    public async Task<Response> Handle(
+    public async Task<Response<ProductCreationResponse>> Handle(
         ProductCreationRequest request,
         CancellationToken cancellationToken
     )
     {
         var validationResult = await validator.ValidateAsync(request);
         if (!validationResult.IsValid)
-            return new ValidationFailureResponse(errors: validationResult.Errors);
+            return new ValidationFailureResponse<ProductCreationResponse>(errors: validationResult.Errors);
 
         var category = await categoryRepository.RetrieveByIdAsync(request.CategoryId);
         if (category is null)
-            return new Response(
+            return new Response<ProductCreationResponse>(
+                data: null,
                 statusCode: StatusCodes.Status404NotFound,
                 message: "Category not found."
             );
@@ -27,12 +28,15 @@ public sealed class ProductCreationHandler(
         var product = TinyMapper.Map<Product>(request);
         product.Category = category;
 
+        await productRepository.SaveAsync(product);
+
         var ingredients = new List<ProductIngredient>();
         foreach (var payload in request.Ingredients)
         {
             var ingredient = await ingredientRepository.RetrieveByIdAsync(payload.IngredientId);
             if (ingredient is null)
-                return new Response(
+                return new Response<ProductCreationResponse>(
+                    data: null,
                     statusCode: StatusCodes.Status404NotFound,
                     message: $"Ingredient with ID '{payload.IngredientId}' not found."
                 );
@@ -50,11 +54,12 @@ public sealed class ProductCreationHandler(
             product.Ingredients.Add(ingredient);
         }
 
-        await productRepository.SaveAsync(product);
+        await productRepository.UpdateAsync(product);
 
         logger.LogInformation("Product '{Title}' created successfully.", product.Title);
 
-        return new Response(
+        return new Response<ProductCreationResponse>(
+            data: new ProductCreationResponse { ProductId = product.Id },
             statusCode: StatusCodes.Status201Created,
             message: "Product created successfully."
         );
