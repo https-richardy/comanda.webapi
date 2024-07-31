e8fde07d837f3dfab01955b9be9b3beb37b4eb0d
https-richardy <code.richardy@gmail.com>
2024-07-31 10:59:12 -0300

    added new repository interface: added interface inheriting contracts from IMinimalRepository<TEntity> to ProductIngredient.

---
+++ Source/Data/Repositories/Interfaces/IProductIngredientRepository.cs
@@ -0,0 +1,6 @@
+namespace Comanda.WebApi.Data.Repositories;
+
+public interface IProductIngredientRepository : IMinimalRepository<ProductIngredient>
+{
+    /* This interface inherits the contracts from IMinimalRepository. */
+}
