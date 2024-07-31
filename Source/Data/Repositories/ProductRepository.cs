462f4fa62b5e46530d3b4148e50df468a63bc970
https-richardy <code.richardy@gmail.com>
2024-07-31 14:34:02 -0300

    updated: product repository had its 'PagedAsync' and 'PagedAsync' methods with signature receiving a predicate were overloaded to include the navigation properties 'Ingredients' and 'Category'.

--- Source/Data/Repositories/ProductRepository.cs
+++ Source/Data/Repositories/ProductRepository.cs
@@ -4,5 +4,30 @@ public sealed class ProductRepository(ComandaDbContext dbContext) :
     Repository<Product, ComandaDbContext>(dbContext),
     IProductRepository
 {
+    public override  async Task<IEnumerable<Product>> PagedAsync(int pageNumber, int pageSize)
+    {
+        /* Represents the adjustment applied to page numbers to align with zero-based indices in LINQ queries. */
+        const int pageIndexAdjustment = 1;
 
+        return await _dbContext.Products
+            .Include(product => product.Category)
+            .Include(product => product.Ingredients)
+            .Skip((pageNumber - pageIndexAdjustment) * pageSize)
+            .Take(pageSize)
+            .ToListAsync();
+    }
+
+    public override async Task<IEnumerable<Product>> PagedAsync(Expression<Func<Product, bool>> predicate, int pageNumber, int pageSize)
+    {
+        /* Represents the adjustment applied to page numbers to align with zero-based indices in LINQ queries. */
+        const int pageIndexAdjustment = 1;
+
+        return await _dbContext.Products
+            .Include(product => product.Category)
+            .Include(product => product.Ingredients)
+            .Where(predicate)
+            .Skip((pageNumber - pageIndexAdjustment) * pageSize)
+            .Take(pageSize)
+            .ToListAsync();
+    }
 }
