48c980f321482ee42b69cf5c87ffc412c87d51e5
https-richardy <code.richardy@gmail.com>
2024-08-01 10:11:56 -0300

    updated to implement the IRequest<T> interface supporting the mediator.

--- Source/Payloads/Requests/AdditionalPayloads/AdditionalsListingByCategoryRequest.cs
+++ Source/Payloads/Requests/AdditionalPayloads/AdditionalsListingByCategoryRequest.cs
@@ -1,6 +1,7 @@
 namespace Comanda.WebApi.Payloads;
 
 public sealed record AdditionalsListingByCategoryRequest
+    : IRequest<Response<IEnumerable<Additional>>>
 {
     public int CategoryId { get; init; }
 }
