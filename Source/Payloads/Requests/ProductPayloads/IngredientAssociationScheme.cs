670ca8ee30afae7b414f4f01e1b44bdcfd8a503b
https-richardy <code.richardy@gmail.com>
2024-08-01 09:56:02 -0300

    updated: added 'IsMandatory' property.

--- Source/Payloads/Requests/ProductPayloads/IngredientAssociationScheme.cs
+++ Source/Payloads/Requests/ProductPayloads/IngredientAssociationScheme.cs
@@ -4,4 +4,5 @@ public sealed record IngredientAssociationScheme
 {
     public int StandardQuantity { get; init; }
     public int IngredientId { get; init; }
+    public bool IsMandatory { get; init; }
 }
