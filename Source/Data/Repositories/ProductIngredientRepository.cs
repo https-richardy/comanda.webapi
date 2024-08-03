1149ba362aa33147dba6b2848031da817f764342
https-richardy <code.richardy@gmail.com>
2024-08-03 15:16:15 -0300

    method for retrieving the ingredient of a specific product has been overridden to include the "Ingredient" navigation property.

--- Source/Data/Repositories/ProductIngredientRepository.cs
+++ Source/Data/Repositories/ProductIngredientRepository.cs
@@ -4,5 +4,12 @@ public sealed class ProductIngredientRepository(ComandaDbContext dbContext) :
     MinimalRepository<ProductIngredient, ComandaDbContext>(dbContext),
     IProductIngredientRepository
 {
-
+    #pragma warning disable CS8603
+    public override async Task<ProductIngredient> RetrieveByIdAsync(int id)
+    {
+        return await _dbContext.ProductIngredients
+            .Include(productIngredient => productIngredient.Ingredient)
+            .Where(productIgredient => productIgredient.Id == id)
+            .FirstOrDefaultAsync();
+    }
 }
