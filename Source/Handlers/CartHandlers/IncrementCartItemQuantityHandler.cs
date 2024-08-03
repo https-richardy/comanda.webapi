5c1d334ab292545eaf7265e5b38e6acf40169e7b
https-richardy <code.richardy@gmail.com>
2024-08-03 12:24:12 -0300

    new handler added: handler for requesting to increase the quantity of an item in the cart has been added.

---
+++ Source/Handlers/CartHandlers/IncrementCartItemQuantityHandler.cs
@@ -0,0 +1,49 @@
+namespace Comanda.WebApi.Handlers;
+
+public sealed class IncrementCartItemQuantityHandler(
+    ICartRepository cartRepository,
+    ICustomerRepository customerRepository,
+    IUserContextService userContextService
+) : IRequestHandler<IncrementCartItemQuantityRequest, Response>
+{
+    public async Task<Response> Handle(
+        IncrementCartItemQuantityRequest request,
+        CancellationToken cancellationToken
+    )
+    {
+        var userIdentifier = userContextService.GetCurrentUserIdentifier();
+
+        /*
+            It is impossible for the 'userIdentifier' to be null at this point
+            since the 'CartController' restricts access only to authenticated customers.
+        */
+        #pragma warning disable CS8604, CS8602
+
+        var customer = await customerRepository.FindCustomerByUserIdAsync(userIdentifier);
+
+        /*
+            Even here it can't be null because when we create an account
+            we have already created and associated a customer with it.
+        */
+
+        var cart = await cartRepository.FindCartWithItemsAsync(customer.Id);
+
+        var itemToIncrement = cart.Items
+            .FirstOrDefault(item => item.Product.Id == request.ItemId);
+
+        if (itemToIncrement is null)
+            return new Response(
+                statusCode: StatusCodes.Status404NotFound,
+                message: "Cart item not found."
+            );
+
+        itemToIncrement.Quantity++;
+
+        await cartRepository.UpdateAsync(cart);
+
+        return new Response(
+            statusCode: StatusCodes.Status200OK,
+            message: "Cart item quantity updated successfully."
+        );
+    }
+}
