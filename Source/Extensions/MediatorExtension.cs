adb4c43d21e25eac43e9b2a78213c2c419cf2d49
https-richardy <code.richardy@gmail.com>
2024-08-03 12:06:06 -0300

    handler for requesting an increase in the quantity of the cart item registered in the service collection.

--- Source/Extensions/MediatorExtension.cs
+++ Source/Extensions/MediatorExtension.cs
@@ -21,6 +21,7 @@ public static class MediatorExtension
         services.AddScoped<IRequestHandler<InsertProductIntoCartRequest, Response>, InsertProductToCartHandler>();
         services.AddScoped<IRequestHandler<GetCartDetailsRequest, Response<CartResponse>>, CartDetailHandler>();
         services.AddScoped<IRequestHandler<UpdateItemQuantityInCartRequest, Response>, UpdateCartItemQuantityHandler>();
+        services.AddScoped<IRequestHandler<IncrementCartItemQuantityRequest, Response>, IncrementCartItemQuantityHandler>();
         services.AddScoped<IRequestHandler<ModifyCartItemQuantityRequest, Response>, ModifyItemQuantityHandler>();
 
         #endregion
