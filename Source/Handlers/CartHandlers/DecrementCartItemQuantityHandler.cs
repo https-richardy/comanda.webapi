95e8ec5942421d2b570464a799a9896ff6b9c0f9
https-richardy <code.richardy@gmail.com>
2024-08-03 12:25:51 -0300

    new handler added: handler for requesting a decrease in the quantity of an item in the cart has been added.

---
+++ Source/Handlers/CartHandlers/DecrementCartItemQuantityHandler.cs
@@ -0,0 +1,55 @@
+namespace Comanda.WebApi.Handlers;
+
+public sealed class DecrementCartItemQuantityHandler(
+    ICartRepository cartRepository,
+    ICustomerRepository customerRepository,
+    IUserContextService userContextService
+) : IRequestHandler<DecrementCartItemQuantityRequest, Response>
+{
+    public async Task<Response> Handle(
+        DecrementCartItemQuantityRequest request,
+        CancellationToken cancellationToken
+    )
+    {
+        var userIdentifier = userContextService.GetCurrentUserIdentifier();
+
+        /*
+            It is impossible for the 'userIdentifier' to be null at this point
+            since the 'CartController' restricts access only to authenticated customers.
+        */
+        #pragma warning disable CS8604, CS8602
+
+        var customer = await customerRepository.FindCustomerByUserIdAsync(userIdentifier);
+
+        /*
+            Even here it can't be null because when we create an account
+            we have already created and associated a customer with it.
+        */
+
+        var cart = await cartRepository.FindCartWithItemsAsync(customer.Id);
+
+        var itemToDecrement = cart.Items
+            .FirstOrDefault(item => item.Product.Id == request.ItemId);
+
+        if (itemToDecrement is null)
+            return new Response(
+                statusCode: StatusCodes.Status404NotFound,
+                message: "Cart item not found."
+            );
+
+        if (itemToDecrement.Quantity == 1)
+            return new Response(
+                statusCode: StatusCodes.Status400BadRequest,
+                message: "porra."
+            );
+
+        itemToDecrement.Quantity--;
+
+        await cartRepository.UpdateAsync(cart);
+
+        return new Response(
+            statusCode: StatusCodes.Status200OK,
+            message: "Cart item quantity updated successfully."
+        );
+    }
+}
