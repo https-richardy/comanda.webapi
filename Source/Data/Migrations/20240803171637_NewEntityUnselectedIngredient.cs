705df3849d6d86f317fca93b613611d3438d3161
https-richardy <code.richardy@gmail.com>
2024-08-03 14:16:51 -0300

    new database migrations: This migration brings some changes: A new entity has been introduced 'UnselectedIngredient', a relationship with many 'UnselectedIngredient' has been added to CarItem and the relationship between 1 product with additionals has been removed.

---
+++ Source/Data/Migrations/20240803171637_NewEntityUnselectedIngredient.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Comanda.WebApi.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewEntityUnselectedIngredient : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Additionals_Products_ProductId",
+                table: "Additionals");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Additionals_ProductId",
+                table: "Additionals");
+
+            migrationBuilder.DropColumn(
+                name: "ProductId",
+                table: "Additionals");
+
+            migrationBuilder.CreateTable(
+                name: "UnselectedIngredients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IngredientId = table.Column<int>(type: "int", nullable: false),
+                    CartItemId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UnselectedIngredients", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UnselectedIngredients_CartItems_CartItemId",
+                        column: x => x.CartItemId,
+                        principalTable: "CartItems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UnselectedIngredients_Ingredients_IngredientId",
+                        column: x => x.IngredientId,
+                        principalTable: "Ingredients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UnselectedIngredients_CartItemId",
+                table: "UnselectedIngredients",
+                column: "CartItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UnselectedIngredients_IngredientId",
+                table: "UnselectedIngredients",
+                column: "IngredientId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UnselectedIngredients");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProductId",
+                table: "Additionals",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Additionals_ProductId",
+                table: "Additionals",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Additionals_Products_ProductId",
+                table: "Additionals",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id");
+        }
+    }
+}
