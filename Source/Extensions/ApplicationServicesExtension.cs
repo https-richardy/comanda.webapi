02cf43d318829d9a680742f6a2516875ab1177fc
https-richardy <code.richardy@gmail.com>
2024-08-06 13:45:57 -0300

    this change registers the host information provider service in the service collection.

--- Source/Extensions/ApplicationServicesExtension.cs
+++ Source/Extensions/ApplicationServicesExtension.cs
@@ -13,6 +13,7 @@ public static class ApplicationServicesExtension
 
         services.AddFileUploadService();
 
+        services.AddScoped<IHostInformationProvider, HostInformationProvider>();
         services.AddScoped<IAuthenticationService, AuthenticationService>();
         services.AddScoped<IUserContextService, UserContextService>();
     }
