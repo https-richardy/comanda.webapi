44061a049fc651ce97637ce7940d5dff11eda31b
https-richardy <code.richardy@gmail.com>
2024-08-06 16:10:50 -0300

    This change makes the calculation of the order total based on the items in the `Items` list because they now support customization, such as adding additional items to the order.

--- Source/Entities/Order.cs
+++ Source/Entities/Order.cs
@@ -2,11 +2,11 @@ namespace Comanda.WebApi.Entities;
 
 public sealed class Order : Entity
 {
-    public decimal Total => CalculateTotal();
+    public decimal Total => Items.Sum(item => item.Total);
     public Customer Customer { get; set; }
     public Address ShippingAddress { get; set; }
 
-    public ICollection<OrderItem> OrderItems { get; set; } = [];
+    public ICollection<OrderItem> Items { get; set; } = [];
     public EOrderStatus Status { get; set; }
     public DateTime Date { get; set; }
 
@@ -16,10 +16,5 @@ public sealed class Order : Entity
             Default parameterless constructor included due to Entity Framework Core not setting navigation properties
             when using constructors. For more information, see: https://learn.microsoft.com/pt-br/ef/core/modeling/constructors
         */
-    }
-
-    private decimal CalculateTotal()
-    {
-        return OrderItems.Sum(item => item.Product.Price * item.Quantity);
     }
 }
