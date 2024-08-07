1812cc4c6599ec2221d0e8487373f0e6bccaeb4a
https-richardy <code.richardy@gmail.com>
2024-08-07 01:51:54 -0300

    adds a new "Settings" entity to the database, with the following properties

--- Source/Data/Migrations/20240807045048_NewEntitySettings.Designer.cs
+++ Source/Data/Migrations/20240807045048_NewEntitySettings.Designer.cs
@@ -4,6 +4,7 @@ using Comanda.WebApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Comanda.WebApi.Data.Migrations
 {
     [DbContext(typeof(ComandaDbContext))]
-    partial class ComandaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240807045048_NewEntitySettings")]
+    partial class NewEntitySettings
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Source/Data/Migrations/20240807045048_NewEntitySettings.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Comanda.WebApi.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewEntitySettings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Settings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AcceptAutomatically = table.Column<bool>(type: "bit", nullable: false, defaultValue: false),
+                    MaxConcurrentAutomaticOrders = table.Column<int>(type: "int", nullable: false, defaultValue: 5),
+                    EstimatedDeliveryTimeInMinutes = table.Column<int>(type: "int", nullable: false, defaultValue: 30),
+                    DeliveryFee = table.Column<decimal>(type: "decimal(18,2)", nullable: false, defaultValue: 0.0m)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Settings", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Settings",
+                columns: new[] { "Id", "EstimatedDeliveryTimeInMinutes", "MaxConcurrentAutomaticOrders" },
+                values: new object[] { 1, 30, 5 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Settings");
+        }
+    }
+}
