cdc7f4115e4b9a79d9b7726e6b130bf13aa1cdae
https-richardy <code.richardy@gmail.com>
2024-08-07 14:40:09 -0300

    refactors the logic of the successful payment handler to use MediatR. The handler now sends messages to MediatR to retrieve the cart, the delivery address and process the order.

--- Source/Handlers/CheckoutHandlers/SuccessfulPaymentHandler.cs
+++ Source/Handlers/CheckoutHandlers/SuccessfulPaymentHandler.cs
@@ -1,66 +1,24 @@
 namespace Comanda.WebApi.Handlers;
 
-public sealed class SuccessfulPaymentHandler(
-    ICartRepository cartRepository,
-    IOrderRepository orderRepository,
-    IAddressRepository addressRepository,
-    ICheckoutManager checkoutManager,
-    ISettingsRepository settingsRepository
-) : IRequestHandler<SuccessfulPaymentRequest, Response>
+public sealed class SuccessfulPaymentHandler(IMediator mediator) : 
+    IRequestHandler<SuccessfulPaymentRequest, Response>
 {
     public async Task<Response> Handle(
         SuccessfulPaymentRequest request,
         CancellationToken cancellationToken
     )
     {
-        var session = await checkoutManager.GetSessionAsync(request.SessionId);
+        var fetchPaymentSessionRequest = new FetchPaymentSessionRequest(request.SessionId);
+        var session = await mediator.Send(fetchPaymentSessionRequest);
 
         var cartId = int.Parse(session.Metadata["cartId"]);
         var shippingAddressId = int.Parse(session.Metadata["shippingAddressId"]);
 
-        var cart = await cartRepository.RetrieveByIdAsync(cartId);
-        var address = await addressRepository.RetrieveByIdAsync(shippingAddressId);
+        var retrieveCartAndAddressRequest = new RetrieveCartAndAddressRequest(cartId, shippingAddressId);
+        var (cart, address) = await mediator.Send(retrieveCartAndAddressRequest);
 
-        var settings = await settingsRepository.GetSettingsAsync();
-
-        var orderStatus = EOrderStatus.Pending;
-        if (settings.AcceptAutomatically)
-        {
-            var currentPendingOrders = await orderRepository.FindAllAsync(orders => orders.Status == EOrderStatus.Pending);
-            if (currentPendingOrders.Count() < settings.MaxConcurrentAutomaticOrders)
-            {
-                orderStatus = EOrderStatus.Processing;
-            }
-        }
-
-        /* manual mapping of a CartItem to an OrderItem. I have a deadline, but I swear I'll make it more readable.*/
-        var items = cart.Items.Select(cartItem => new OrderItem
-        {
-            Quantity = cartItem.Quantity,
-            Product = cartItem.Product,
-            Additionals = cartItem.Additionals.Select(additional => new OrderItemAdditional
-            {
-                Additional = additional.Additional,
-                Quantity = additional.Quantity
-            }).ToList(),
-            UnselectedIngredients = cartItem.UnselectedIngredients.Select(ingredient => new UnselectedIngredient
-            {
-                Ingredient = ingredient.Ingredient,
-                CartItem = cartItem
-            }).ToList()
-        }).ToList();
-
-        var order = new Order
-        {
-            Items = items,
-            Customer = cart.Customer,
-            ShippingAddress = address,
-            Status = orderStatus,
-            Date = DateTime.Now
-        };
-
-        await orderRepository.SaveAsync(order);
-        await cartRepository.ClearCartAsync(cart);
+        var orderProcessingRequest = new OrderProcessingRequest { Cart = cart, Address = address };
+        await mediator.Send(orderProcessingRequest);
 
         return new Response(
             statusCode: StatusCodes.Status200OK,
