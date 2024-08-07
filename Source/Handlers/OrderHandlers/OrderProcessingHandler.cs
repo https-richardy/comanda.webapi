ee086be2bd34831c8ada6a800cc85bb05327e4c2
https-richardy <code.richardy@gmail.com>
2024-08-07 14:09:01 -0300

    added new handler: new order processing handler.

---
+++ Source/Handlers/OrderHandlers/OrderProcessingHandler.cs
@@ -0,0 +1,54 @@
+namespace Comanda.WebApi.Handlers;
+
+public sealed class OrderProcessingHandler(
+    IOrderRepository orderRepository,
+    ISettingsRepository settingsRepository
+) : IRequestHandler<OrderProcessingRequest, Order>
+{
+    public async Task<Order> Handle(
+        OrderProcessingRequest request,
+        CancellationToken cancellationToken
+    )
+    {
+        var settings = await settingsRepository.GetSettingsAsync();
+
+        var orderStatus = EOrderStatus.Pending;
+        if (settings.AcceptAutomatically)
+        {
+            var currentPendingOrders = await orderRepository.FindAllAsync(orders => orders.Status == EOrderStatus.Pending);
+            if (currentPendingOrders.Count() < settings.MaxConcurrentAutomaticOrders)
+            {
+                orderStatus = EOrderStatus.Processing;
+            }
+        }
+
+        var items = request.Cart.Items.Select(cartItem => new OrderItem
+        {
+            Quantity = cartItem.Quantity,
+            Product = cartItem.Product,
+            Additionals = cartItem.Additionals.Select(additional => new OrderItemAdditional
+            {
+                Additional = additional.Additional,
+                Quantity = additional.Quantity
+            }).ToList(),
+            UnselectedIngredients = cartItem.UnselectedIngredients.Select(ingredient => new UnselectedIngredient
+            {
+                Ingredient = ingredient.Ingredient,
+                CartItem = cartItem
+            }).ToList()
+        }).ToList();
+
+        var order = new Order
+        {
+            Items = items,
+            Customer = request.Cart.Customer,
+            ShippingAddress = request.Address,
+            Status = orderStatus,
+            Date = DateTime.Now
+        };
+
+        await orderRepository.SaveAsync(order);
+
+        return order;
+    }
+}
