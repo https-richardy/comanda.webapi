003dba8fc99442872f2a6cd3cb60b73f13653e8a
https-richardy <code.richardy@gmail.com>
2024-08-07 13:19:38 -0300

    adds an endpoint for updating settings, including authorization for administrators. Allows administrators to change application settings via a PUT request.

--- Source/Controllers/SettingsController.cs
+++ Source/Controllers/SettingsController.cs
@@ -2,12 +2,20 @@ namespace Comanda.WebApi.Controllers;
 
 [ApiController]
 [Route("api/settings")]
+[Authorize(Roles = "Administrator")]
 public sealed class SettingsController(IMediator mediator) : ControllerBase
 {
     [HttpGet]
     public async Task<IActionResult> GetSettingsAsync()
     {
         var response = await mediator.Send((SettingsDetailsRequest) new());
+        return StatusCode(response.StatusCode, response);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateSettingsAsync(SettingsEditingRequest request)
+    {
+        var response = await mediator.Send(request);
         return StatusCode(response.StatusCode, response);
     }
 }
