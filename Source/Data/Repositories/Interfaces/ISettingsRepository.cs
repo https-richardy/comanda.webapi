5fb8d60a6f4f66dd58140c50af4d670ad0b84041
https-richardy <code.richardy@gmail.com>
2024-08-07 01:36:53 -0300

    implements the `GetSettingsAsync` method in the settings repository to fetch the system settings. This method uses the DbContext to fetch the first available configuration.

--- Source/Data/Repositories/Interfaces/ISettingsRepository.cs
+++ Source/Data/Repositories/Interfaces/ISettingsRepository.cs
@@ -2,5 +2,5 @@ namespace Comanda.WebApi.Data.Repositories;
 
 public interface ISettingsRepository : IRepository<Settings>
 {
-    /* This interface inherits the contracts from IRepository. */
+    Task<Settings> GetSettingsAsync();
 }
--- Source/Data/Repositories/SettingsRepository.cs
+++ Source/Data/Repositories/SettingsRepository.cs
@@ -4,5 +4,10 @@ public sealed class SettingsRepository(ComandaDbContext dbContext) :
     Repository<Settings, ComandaDbContext>(dbContext),
     ISettingsRepository
 {
-
+    #pragma warning disable CS8603
+    public async Task<Settings> GetSettingsAsync()
+    {
+        var settings = await _dbContext.Settings.FirstOrDefaultAsync();
+        return settings;
+    }
 }
