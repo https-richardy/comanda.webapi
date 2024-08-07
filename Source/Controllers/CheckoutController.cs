e325f9c23a593b2fe114cdc564086aece84d9989
https-richardy <code.richardy@gmail.com>
2024-08-07 00:18:11 -0300

    adds the possibility of receiving data from the request to create the checkout session. The `request` parameter is now received in the `CreateCheckoutSessionAsync` action, allowing the data needed to create the session to be sent by the client.

--- Source/Controllers/CheckoutController.cs
+++ Source/Controllers/CheckoutController.cs
@@ -6,9 +6,9 @@ public sealed class CheckoutController(IMediator mediator) : ControllerBase
 {
     [HttpPost]
     [Authorize(Roles = "Customer")]
-    public async Task<IActionResult> CreateCheckoutSessionAsync()
+    public async Task<IActionResult> CreateCheckoutSessionAsync(CheckoutRequest request)
     {
-        var response = await mediator.Send((CheckoutRequest)new());
+        var response = await mediator.Send(request);
         return StatusCode(response.StatusCode, response);
     }
 
