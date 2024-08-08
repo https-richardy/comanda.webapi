6f16eba8299d71e4f7a825bd5db9d1298d02cddd
https-richardy <code.richardy@gmail.com>
2024-08-08 14:34:05 -0300

    fix OrderRepository PagedAsync to Include Related Entities

--- Source/Data/Repositories/OrderRepository.cs
+++ Source/Data/Repositories/OrderRepository.cs
@@ -4,5 +4,18 @@ public sealed class OrderRepository(ComandaDbContext dbContext) :
     Repository<Order, ComandaDbContext>(dbContext),
     IOrderRepository
 {
+    public override async Task<IEnumerable<Order>> PagedAsync(int pageNumber, int pageSize)
+    {
+        /* Represents the adjustment applied to page numbers to align with zero-based indices in LINQ queries. */
+        const int pageIndexAdjustment = 1;
 
+        return await _dbContext.Orders
+            .Include(order => order.Customer)
+            .Include(order => order.ShippingAddress)
+            .Include(order => order.Items)
+            .ThenInclude(item => item.Product)
+            .Skip((pageNumber - pageIndexAdjustment) * pageSize)
+            .Take(pageSize)
+            .ToListAsync();
+    }
 }
