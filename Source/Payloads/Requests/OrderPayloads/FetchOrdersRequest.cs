fa5238c6e73aba074f4a4fe02c2063f80221bfc5
https-richardy <code.richardy@gmail.com>
2024-08-08 13:56:44 -0300

    Added a new record representing the payload of the request to obtain orders.

---
+++ Source/Payloads/Requests/OrderPayloads/FetchOrdersRequest.cs
@@ -0,0 +1,9 @@
+namespace Comanda.WebApi.Payloads;
+
+public sealed record FetchOrdersRequest :
+    IRequest<Response<IEnumerable<FormattedOrder>>>
+{
+    public int PageNumber { get; init; } = 1;
+    public int PageSize { get; init; } = 10;
+    public EOrderStatus StatusFilter = EOrderStatus.Pending;
+}
