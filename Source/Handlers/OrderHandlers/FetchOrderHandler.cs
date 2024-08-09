a6a73732e295f0a450f70174f84965ed55be5d8c
https-richardy <code.richardy@gmail.com>
2024-08-08 23:25:04 -0300

    new handler added: request handler for obtaining details of a request has been added.

---
+++ Source/Handlers/OrderHandlers/FetchOrderHandler.cs
@@ -0,0 +1,26 @@
+namespace Comanda.WebApi.Handlers;
+
+public sealed class FetchOrderHandler(IOrderRepository orderRepository) :
+    IRequestHandler<OrderDetailsRequest, Response<FormattedOrderDetails>>
+{
+    public async Task<Response<FormattedOrderDetails>> Handle(
+        OrderDetailsRequest request,
+        CancellationToken cancellationToken
+    )
+    {
+        var order = await orderRepository.RetrieveByIdAsync(request.OrderId);
+        if (order is null)
+            return new Response<FormattedOrderDetails>(
+                data: null,
+                statusCode: StatusCodes.Status404NotFound,
+                message: $"order with ID '{request.OrderId}' was not found."
+            );
+
+        var formattedOrder = (FormattedOrderDetails)order;
+        return new Response<FormattedOrderDetails>(
+            data: formattedOrder,
+            statusCode: StatusCodes.Status404NotFound,
+            message: "the request was successfully recovered"
+        );
+    }
+}
