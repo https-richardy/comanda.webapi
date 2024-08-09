5745dd58f499c783b3af199c78ab4d289dda6c14
https-richardy <code.richardy@gmail.com>
2024-08-08 21:46:39 -0300

    new handler added: request handler for updating the status of an order has been added

---
+++ Source/Handlers/OrderHandlers/SetOrderStatusHandler.cs
@@ -0,0 +1,27 @@
+namespace Comanda.WebApi.Handlers;
+
+public sealed class SetOrderStatusHandler(
+    IOrderRepository orderRepository
+) : IRequestHandler<SetOrderStatusRequest, Response>
+{
+    public async Task<Response> Handle(
+        SetOrderStatusRequest request,
+        CancellationToken token
+    )
+    {
+        var order = await orderRepository.RetrieveByIdAsync(request.OrderId);
+        if (order is null)
+            return new Response(
+                statusCode: StatusCodes.Status404NotFound,
+                message: "order with ID '{request.OrderId}' was not found."
+            );
+
+        order.Status = request.Status;
+        await orderRepository.UpdateAsync(order);
+
+        return new Response(
+            statusCode: StatusCodes.Status200OK,
+            message: "order status has been updated successfully."
+        );
+    }
+}
