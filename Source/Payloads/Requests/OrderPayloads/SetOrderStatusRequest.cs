85187b28f2b4ae92dd6d26551cbef8eb2de14391
https-richardy <code.richardy@gmail.com>
2024-08-08 21:53:43 -0300

    changes the access modifier of the `OrderId` property from `init` to `set` to allow the Id to be set.

--- Source/Payloads/Requests/OrderPayloads/SetOrderStatusRequest.cs
+++ Source/Payloads/Requests/OrderPayloads/SetOrderStatusRequest.cs
@@ -3,6 +3,6 @@ namespace Comanda.WebApi.Payloads;
 public sealed record SetOrderStatusRequest : IRequest<Response>
 {
     [JsonIgnore] /* this property will be set from the route. */
-    public int OrderId { get; init; }
+    public int OrderId { get; set; }
     public EOrderStatus Status { get; init; }
 }
