199fb99b75c020f05c8a4a52b4a8d6851c04afb7
https-richardy <code.richardy@gmail.com>
2024-08-09 15:04:02 -0300

    adds a text column to the database to record the reason for canceling an order. This will allow better control and monitoring of cancellations, making it easier to analyze and identify patterns and areas for improvement.

--- Source/Data/Migrations/20240809175940_NewColumnCancelledReason.Designer.cs
+++ Source/Data/Migrations/20240809175940_NewColumnCancelledReason.Designer.cs
@@ -4,6 +4,7 @@ using Comanda.WebApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Comanda.WebApi.Data.Migrations
 {
     [DbContext(typeof(ComandaDbContext))]
-    partial class ComandaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240809175940_NewColumnCancelledReason")]
+    partial class NewColumnCancelledReason
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
