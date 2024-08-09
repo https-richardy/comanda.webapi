86a9f3a3b25df806b9f18ed93fc4beb05351856b
https-richardy <code.richardy@gmail.com>
2024-08-09 11:33:17 -0300

    adds a new `FindByOrderIdAsync` method to the payment repository to fetch a payment by order ID.

--- Source/Data/Repositories/PaymentRepository.cs
+++ Source/Data/Repositories/PaymentRepository.cs
@@ -4,5 +4,11 @@ public sealed class PaymentRepository(ComandaDbContext dbContext) :
     Repository<Payment, ComandaDbContext>(dbContext),
     IPaymentRepository
 {
-
+    #pragma warning disable CS8603
+    public async Task<Payment> FindByOrderIdAsync(int orderId)
+    {
+        return await _dbContext.Payments
+            .Include(payment => payment.Order)
+            .FirstOrDefaultAsync(payment => payment.Order.Id == orderId);
+    }
 }
