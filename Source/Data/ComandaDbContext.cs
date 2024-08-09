283781fa821e46bcf9e1744bc32a0b179af9cd5e
https-richardy <code.richardy@gmail.com>
2024-08-09 01:14:20 -0300

    adds the `Payment` entity to the `ComandaDbContext` data context, allowing the persistence of information about payments made.

--- Source/Data/ComandaDbContext.cs
+++ Source/Data/ComandaDbContext.cs
@@ -10,6 +10,7 @@ public sealed class ComandaDbContext(DbContextOptions options) : IdentityDbConte
     public DbSet<ProductIngredient> ProductIngredients { get; set; }
     public DbSet<Additional> Additionals { get; set; }
     public DbSet<Order> Orders { get; set; }
+    public DbSet<Payment> Payments { get; set; }
     public DbSet<OrderItem> OrderItems { get; set; }
     public DbSet<Cart> Carts { get; set; }
     public DbSet<CartItem> CartItems { get; set; }
