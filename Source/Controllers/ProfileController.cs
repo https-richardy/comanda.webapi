e02eebe330d2f27f608de237561beba52f65f987
https-richardy <code.richardy@gmail.com>
2024-08-09 16:54:41 -0300

    implements a new endpoint in the API so that customers can consult the history of their orders. The `/orders/history` endpoint returns a list of previous orders, allowing customers to view their previous orders.

--- Source/Controllers/ProfileController.cs
+++ Source/Controllers/ProfileController.cs
@@ -11,4 +11,12 @@ public sealed class ProfileController(IMediator mediator) : ControllerBase
         var response = await mediator.Send(request);
         return StatusCode(response.StatusCode, response);
     }
+
+    [HttpGet("orders/history")]
+    [Authorize(Roles = "Customer")]
+    public async Task<IActionResult> GetOrderHistoryAsync([FromQuery] CustomerOrderHistoryRequest request)
+    {
+        var response = await mediator.Send(request);
+        return StatusCode(response.StatusCode, response);
+    }
 }
