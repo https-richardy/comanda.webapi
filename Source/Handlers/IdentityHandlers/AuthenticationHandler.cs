313a03ad720d79b2faa4a647460ca9146132a264
https-richardy <code.richardy@gmail.com>
2024-08-10 18:24:04 -0300

    this change removes unnecessary private fields in the `AuthenticationHandler` class, making the code cleaner. Dependencies are now injected directly into the p constructor and accessed directly, eliminating the need for private fields.

--- Source/Handlers/IdentityHandlers/AuthenticationHandler.cs
+++ Source/Handlers/IdentityHandlers/AuthenticationHandler.cs
@@ -4,20 +4,15 @@ public sealed class AuthenticationHandler(
     UserManager<Account> userManager,
     IAuthenticationService authenticationService,
     IJwtService jwtService
-) :
-    IRequestHandler<AuthenticationCredentials, Response<AuthenticationResponse>>
+) : IRequestHandler<AuthenticationCredentials, Response<AuthenticationResponse>>
 {
-    private readonly UserManager<Account> _userManager = userManager;
-    private readonly IAuthenticationService _authenticationService = authenticationService;
-    private readonly IJwtService _jwtService = jwtService;
-
     # pragma warning disable CS8604
     public async Task<Response<AuthenticationResponse>> Handle(
         AuthenticationCredentials request,
         CancellationToken cancellationToken
     )
     {
-        var validCredentials = await _authenticationService.ValidateCredentialsAsync(request);
+        var validCredentials = await authenticationService.ValidateCredentialsAsync(request);
         if (validCredentials is false)
             return new Response<AuthenticationResponse>(
                 data: null,
@@ -25,10 +20,10 @@ public sealed class AuthenticationHandler(
                 message: "invalid credentials."
             );
 
-        var account = await _userManager.FindByEmailAsync(request.Email);
-        var claimsIdentity = await _authenticationService.BuildClaimsIdentity(account);
+        var account = await userManager.FindByEmailAsync(request.Email);
+        var claimsIdentity = await authenticationService.BuildClaimsIdentity(account);
 
-        var token = _jwtService.GenerateToken(claimsIdentity);
+        var token = jwtService.GenerateToken(claimsIdentity);
 
         var response = new AuthenticationResponse { Token = token };
         return new Response<AuthenticationResponse>(
