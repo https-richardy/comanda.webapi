0e0ca9e19d56e0887b43e945790fb4a726abfac5
https-richardy <code.richardy@gmail.com>
2024-08-10 13:45:38 -0300

    adds a new handler: adds a request handler for deleting the customer's address.

---
+++ Source/Handlers/ProfileHandlers/AddressDeletionHandler.cs
@@ -0,0 +1,39 @@
+namespace Comanda.WebApi.Handlers;
+
+public sealed class AddressDeletionHandler(
+    IUserContextService userContextService,
+    ICustomerRepository customerRepository
+) : IRequestHandler<AddressDeletionRequest, Response>
+{
+    public async Task<Response> Handle(
+        AddressDeletionRequest request,
+        CancellationToken cancellationToken
+    )
+    {
+        /*
+            It is impossible for the 'userIdentifier' to be null at this point
+            since the 'ProfileController' restricts access only to authenticated customers.
+        */
+        #pragma warning disable CS8604, CS8602
+
+        var userIdentifier = userContextService.GetCurrentUserIdentifier();
+        var customer = await customerRepository.FindCustomerByUserIdAsync(userIdentifier);
+
+        var address = customer.Addresses.FirstOrDefault(address => address.Id == request.AddressId);
+        if (address is null)
+        {
+            return new Response(
+                statusCode: StatusCodes.Status404NotFound,
+                message: "address not found."
+            );
+        }
+
+        customer.Addresses.Remove(address);
+        await customerRepository.UpdateAsync(customer);
+
+        return new Response(
+            statusCode: StatusCodes.Status200OK,
+            message: "address successfully deleted."
+        );
+    }
+}
