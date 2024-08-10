d60472d5e890d8a116a571d02ab88443acdec13a
https-richardy <code.richardy@gmail.com>
2024-08-09 23:42:31 -0300

    moving the `CustomerOrderHistoryRequest` class from the `OrderPayloads` folder to `ProfilePayloads`.  The class is now part of the customer profile context, better reflecting its functionality.

--- Source/Payloads/Requests/ProfilePayloads/CustomerOrderHistoryRequest.cs
+++ Source/Payloads/Requests/ProfilePayloads/CustomerOrderHistoryRequest.cs
@@ -5,11 +5,4 @@ public sealed record CustomerOrderHistoryRequest :
 {
     public int PageSize { get; init; } = 10;
     public int PageNumber { get; init; } = 1;
-
-    public DateTime? StartDate { get; init; }
-    public DateTime? EndDate { get; init; }
-    public EOrderStatus? Status { get; init; }
-
-    public decimal? MinPrice { get; init; }
-    public decimal? MaxPrice { get; init; }
 }
