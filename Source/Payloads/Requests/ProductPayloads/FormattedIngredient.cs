a40bc87a7f319d12d8a24f993a3858761510da63
https-richardy <code.richardy@gmail.com>
2024-08-16 14:17:28 -0300

    adds a new record representing a payload called `FormattedIngredient` to represent ingredients in a format suitable for display. This payload includes information such as the name, the default quantity and whether the ingredient is mandatory.

---
+++ Source/Payloads/Requests/ProductPayloads/FormattedIngredient.cs
@@ -0,0 +1,29 @@
+namespace Comanda.WebApi.Payloads;
+
+public record class FormattedIngredient
+{
+    public int Id { get; init; }
+    public string Name { get; init; }
+    public int StandardQuantity { get; init; }
+    public bool IsMandatory { get; init; }
+
+    public FormattedIngredient(int id, string name, int standardQuantity, bool isMandatory)
+    {
+        Id = id;
+        Name = name;
+        StandardQuantity = standardQuantity;
+        IsMandatory = isMandatory;
+    }
+
+    public static implicit operator FormattedIngredient(ProductIngredient productIngredient)
+    {
+        var ingredient = productIngredient.Ingredient;
+
+        return new FormattedIngredient(
+            id: ingredient.Id,
+            name: ingredient.Name,
+            standardQuantity: productIngredient.StandardQuantity,
+            isMandatory: productIngredient.IsMandatory
+        );
+    }
+}
