b50fbd0504ce83b90e285568128984396819e9e0
https-richardy <code.richardy@gmail.com>
2024-08-21 17:17:11 -0300

    adds a new record type `SmtpSettings` to configure sending emails via SMTP

---
+++ Source/Services/EmailService/SmtpSettings.cs
@@ -0,0 +1,10 @@
+namespace Comanda.WebApi.Services;
+
+public sealed record SmtpSettings
+{
+    public string Host { get; init; } = null!;
+    public string UserName { get; init; } = null!;
+    public string Password { get; init; } = null!;
+    public int Port { get; init; }
+    public bool UseSsl { get; init; }
+}
