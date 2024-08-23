713859d1ba9a945895ef1eff89d488058fd7c31b
https-richardy <code.richardy@gmail.com>
2024-08-23 16:08:00 -0300

    this commit introduces new data migrations: It implements a confirmation token scheme for cases where it is necessary to send a token, such as: password reset.

---
+++ Source/Data/Migrations/20240823185231_ConfirmationTokenScheme.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Comanda.WebApi.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConfirmationTokenScheme : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ConfirmationTokenId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "ConfirmationToken",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Token = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ExpirationDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ConfirmationToken", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_ConfirmationTokenId",
+                table: "AspNetUsers",
+                column: "ConfirmationTokenId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_ConfirmationToken_ConfirmationTokenId",
+                table: "AspNetUsers",
+                column: "ConfirmationTokenId",
+                principalTable: "ConfirmationToken",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_ConfirmationToken_ConfirmationTokenId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropTable(
+                name: "ConfirmationToken");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_ConfirmationTokenId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ConfirmationTokenId",
+                table: "AspNetUsers");
+        }
+    }
+}
