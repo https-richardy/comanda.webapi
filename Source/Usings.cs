64cb1412ca89081634185eab5de289619667e9fb
https-richardy <code.richardy@gmail.com>
2024-08-27 22:17:09 -0300

    adds a global usage directive for the namespace: System.Diagnostics.CodeAnalysis

--- Source/Usings.cs
+++ Source/Usings.cs
@@ -1,6 +1,7 @@
 /* add global usage directives for System namespaces here */
 
 global using System.Reflection;
+global using System.Diagnostics.CodeAnalysis;
 global using System.Linq.Expressions;
 global using System.Text;
 global using System.Text.Json;
