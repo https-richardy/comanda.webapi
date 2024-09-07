a91c11f5b9374928f228d3d9b71cf14723b0305c
https-richardy <code.richardy@gmail.com>
2024-09-07 14:45:12 -0300

    adds usage directive for namespace System.Net.Http.Headers.

--- Tests/Usings.cs
+++ Tests/Usings.cs
@@ -2,6 +2,7 @@
 
 global using System.Reflection;
 global using System.Net;
+global using System.Net.Http.Headers;
 global using System.Text;
 global using System.Text.Json;
 global using System.Text.Json.Serialization;
