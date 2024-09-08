a6706cba842548bcd0e425965feecd4b7f61c4de
https-richardy <code.richardy@gmail.com>
2024-09-08 18:25:28 -0300

    this commit removes exceptions that are not used in the project.

--- Source/Exceptions/CustomerNotFoundException.cs
+++
@@ -1,9 +0,0 @@
-namespace Comanda.WebApi.Exceptions;
-
-public sealed class CustomerNotFoundException : Exception
-{
-    public CustomerNotFoundException(int customerId)
-        : base($"Customer with ID {customerId} not found.")
-    {
-    }
-}
