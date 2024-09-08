bf008c11a82707d9d98c8efd5bf93212f456ede8
https-richardy <code.richardy@gmail.com>
2024-09-08 18:45:54 -0300

    Adds an implicit conversion from `CouponCreationRequest` to `Coupon` to simplify the creation of coupons. The conversion sets the `IsActive` attribute to `true` by default.

--- Source/Payloads/Requests/CouponPayloads/CouponCreationRequest.cs
+++ Source/Payloads/Requests/CouponPayloads/CouponCreationRequest.cs
@@ -5,4 +5,15 @@ public sealed record CouponCreationRequest : IRequest<Response>
     public string Code { get; set; }
     public decimal Discount { get; set; }
     public ECouponType Type { get; set; }
+
+    public static implicit operator Coupon(CouponCreationRequest request)
+    {
+        return new Coupon
+        {
+            Code = request.Code,
+            Discount = request.Discount,
+            Type = request.Type,
+            IsActive = true
+        };
+    }
 }
