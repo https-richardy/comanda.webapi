a56099b45ebeaee633f3d1655a88390faec86078
https-richardy <code.richardy@gmail.com>
2024-09-08 22:07:20 -0300

    implements an end-to-end test to verify that the list of coupons returns the available coupons.

--- Tests/EndToEnd/CouponEndpoint.EndtoEnd.cs
+++ Tests/EndToEnd/CouponEndpoint.EndtoEnd.cs
@@ -58,6 +58,31 @@ public sealed class CouponEndpointTests : WebApiFixture
         Assert.Contains(responseContent!.Errors, error => error.PropertyName == "ExpirationDate");
     }
 
+    [Fact(DisplayName = "Should return available coupons when requesting the coupon list")]
+    public async Task ShouldReturnAvailableCouponsWhenRequestingTheCouponList()
+    {
+        var client = GetAuthenticatedClient();
+        var payloads = new List<CouponCreationRequest>
+        {
+            new CouponCreationRequest { Code = "TESTCOUPONCODE1", ExpirationDate = DateTime.UtcNow.AddDays(2), Type = ECouponType.Percentage, Discount = 10m },
+            new CouponCreationRequest { Code = "TESTCOUPONCODE2", ExpirationDate = DateTime.UtcNow.AddDays(2), Type = ECouponType.Percentage, Discount = 20m },
+        };
+
+        foreach (var payload in payloads)
+        {
+            var creationResponse = await client.PostAsJsonAsync("api/coupons", payload);
+            creationResponse.EnsureSuccessStatusCode();
+        }
+
+        var response = await client.GetFromJsonAsync<Response<IEnumerable<Coupon>>>("api/coupons");
+
+        Assert.NotNull(response);
+        Assert.NotNull(response.Data);
+
+        Assert.True(response.IsSuccess);
+        Assert.Equal(2, response.Data.Count());
+    }
+
     private async Task AuthenticateAdminUserAsync()
     {
         var payload = new AuthenticationCredentials
