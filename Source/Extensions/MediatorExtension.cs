9c7b53d34d16d0344f13b087f1b157859b4150be
https-richardy <code.richardy@gmail.com>
2024-09-08 22:56:32 -0300

    this commit introduces the coupon code retrieval request handler into the services collection.

--- Source/Extensions/MediatorExtension.cs
+++ Source/Extensions/MediatorExtension.cs
@@ -126,6 +126,7 @@ internal static class MediatorExtension
         services.AddScoped<IRequestHandler<CouponCreationRequest, Response>, CouponCreationHandler>();
         services.AddScoped<IRequestHandler<CouponListingRequest, Response<IEnumerable<Coupon>>>, CouponListingHandler>();
         services.AddScoped<IRequestHandler<FetchCouponByIdentifier, Response<Coupon>>, FetchCouponByIdentifierHandler>();
+        services.AddScoped<IRequestHandler<FetchCouponByCodeRequest, Response<Coupon>>, FetchCouponByCodeHandler>();
 
         #endregion
     }
