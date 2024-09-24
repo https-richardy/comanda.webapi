c0f52a1ac3c5c4a1148e94a6342c55232b85c61a
https-richardy <code.richardy@gmail.com>
2024-09-24 13:34:36 -0300

    this commit introduces the data export service to the services collection.

--- Source/Extensions/ApplicationServicesExtension.cs
+++ Source/Extensions/ApplicationServicesExtension.cs
@@ -26,6 +26,7 @@ internal static class ApplicationServicesExtension
         services.AddScoped<IMenuFormatter, MenuFormatterService>();
         services.AddScoped<IGeminiService, GeminiService>();
         services.AddScoped<IRecommendationService, RecommendationService>();
+        services.AddScoped<IProfileDataExportService, ProfileDataExportService>();
 
         services.AddScoped<IEmailService, SmtpEmailService>(provider =>
         {
