8a1243b7c3c721117009544edebf4a7b2494f12f
https-richardy <code.richardy@gmail.com>
2024-10-26 13:06:51 -0300

    adds an SQL script to populate the categories table with common categories found in cafeterias and fast food establishments.

---
+++ Source/Data/Scripts/Seeds/SeedCommonCategories.sql
@@ -0,0 +1,10 @@
+-- This script is responsible for populating the categories table with
+-- a list of common categories used in cafeterias and fast food establishments.
+-- Adding these categories is essential for organizing the ingredients in different sections of the menu.
+
+INSERT INTO Categories (Name)
+VALUES
+    ('Lanche'),
+    ('Bebidas'),
+    ('Acompanhamentos'),
+    ('Pizzas')
