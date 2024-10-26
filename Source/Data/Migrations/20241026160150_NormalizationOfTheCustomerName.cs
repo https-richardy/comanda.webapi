17e07776edec83c4def81cf9e6ce97e5da2200be
https-richardy <code.richardy@gmail.com>
2024-10-26 13:03:59 -0300

    this commit introduces new data migrations: The new data migration introduces the normalization of the Customer name in the orders table, reducing the dependency on the Customer object.

---
+++ Source/Data/Migrations/20241026160150_NormalizationOfTheCustomerName.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Comanda.WebApi.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class NormalizationOfTheCustomerName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CustomerName",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomerName",
+                table: "Orders");
+        }
+    }
+}
