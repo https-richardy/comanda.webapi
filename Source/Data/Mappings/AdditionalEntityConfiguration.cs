7d1c6085297ea056fa87de2174cb6e050a41eaf8
https-richardy <code.richardy@gmail.com>
2024-10-27 07:32:14 -0300

    this commit introduces the mapping of the Additional entity to the database, configuring the table, primary key, Name and Price properties with their respective types and validations.

---
+++ Source/Data/Mappings/AdditionalEntityConfiguration.cs
@@ -0,0 +1,18 @@
+namespace Comanda.WebApi.Data.Mappings;
+
+public sealed class AdditionalEntityConfiguration : IEntityTypeConfiguration<Additional>
+{
+    public void Configure(EntityTypeBuilder<Additional> builder)
+    {
+        builder.ToTable(nameof(Additional));
+        builder.HasKey(additional => additional.Id);
+
+        builder.Property(additional => additional.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(additional => additional.Price)
+            .IsRequired()
+            .HasPrecision(9, 2);
+    }
+}
