0759ad36d5c7fbd72a71f9f8578eb2db15b1546f
https-richardy <code.richardy@gmail.com>
2024-10-26 22:42:20 -0300

    this commit introduces the refactoring of `CategoryEndpointTests` to use ApiIntegrationBase, ensuring resource release and avoiding unstable tests.

--- Tests/EndToEnd/CategoryEndpoint.EndToEnd.cs
+++ Tests/EndToEnd/CategoryEndpoint.EndToEnd.cs
@@ -1,27 +1,79 @@
 namespace Comanda.TestingSuite.EndToEnd;
 
-public sealed class CategoryEndpointTests : WebApiFixture<ComandaDbContext>
+public sealed class CategoryEndpointTests :
+    IClassFixture<ApiIntegrationBase<Program, ComandaDbContext>>,
+    IAsyncLifetime
 {
-    public CategoryEndpointTests(WebApiFactoryFixture<Program> factory)
-        : base(factory) {  }
+    private readonly HttpClient _httpClient;
+    private readonly IFixture _fixture;
+    private readonly ApiIntegrationBase<Program, ComandaDbContext> _factory;
+
+    public CategoryEndpointTests(ApiIntegrationBase<Program, ComandaDbContext> factory)
+    {
+        _factory = factory;
+
+        _fixture = new Fixture();
+        _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+
+        _httpClient = _factory.CreateClient();
+    }
 
     [Fact(DisplayName = "Given a valid request, it must then create a new category")]
     public async Task GivenAValidRequestItMustThenCreateANewCategory()
     {
-        await AuthenticateAdminUserAsync();
-        var client = GetAuthenticatedClient();
+        var authenticatedClient = await _factory.AuthenticateClientAsync(new AuthenticationCredentials
+        {
+            Email = "comanda@admin.com",
+            Password = "ComandaAdministrator123*"
+        });
 
         var payload = new CategoryCreationRequest
         {
             Title = "Food"
         };
 
-        var response = await client.PostAsJsonAsync("api/categories", payload);
+        var response = await authenticatedClient.PostAsJsonAsync("api/categories", payload);
         var responseContent = await response.Content.ReadFromJsonAsync<Response>();
 
         response.EnsureSuccessStatusCode();
 
         Assert.NotNull(responseContent);
         Assert.True(responseContent.IsSuccess);
+    }
+
+    public async Task DisposeAsync() => await Task.CompletedTask;
+    public async Task InitializeAsync()
+    {
+        _factory.CleanUp();
+
+        var services = _factory.GetServiceProvider();
+
+        var userManager = services.GetService<UserManager<Account>>();
+        var roleManager = services.GetService<RoleManager<IdentityRole>>();
+
+        Assert.NotNull(userManager);
+        Assert.NotNull(roleManager);
+
+        var admin = new Account
+        {
+            UserName = "Comanda Administrator",
+            Email = "comanda@admin.com",
+        };
+
+        var result = await userManager.CreateAsync(admin, "ComandaAdministrator123*");
+        if (result.Succeeded is true)
+        {
+            const string adminRoleName = "Administrator";
+            var adminRoleExists = await roleManager.RoleExistsAsync(adminRoleName);
+            Assert.False(adminRoleExists);
+
+            if (adminRoleExists is false)
+            {
+                var adminRole = new IdentityRole(adminRoleName);
+                await roleManager.CreateAsync(adminRole);
+            }
+
+            await userManager.AddToRoleAsync(admin, adminRoleName);
+        }
     }
 }
