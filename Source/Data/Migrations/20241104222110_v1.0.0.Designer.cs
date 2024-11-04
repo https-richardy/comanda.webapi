370007448f820da77da05050eef0112874a62bcb
https-richardy <code.richardy@gmail.com>
2024-11-04 19:25:52 -0300

    adds new data migrations: version v1.0.0 of the data migrations has been added.

--- Source/Data/Migrations/20241104222110_v1.0.0.Designer.cs
+++ Source/Data/Migrations/20241104222110_v1.0.0.Designer.cs
@@ -4,6 +4,7 @@ using Comanda.WebApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,10 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Comanda.WebApi.Data.Migrations
 {
     [DbContext(typeof(ComandaDbContext))]
-    [ExcludeFromCodeCoverage]
-    partial class ComandaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241104222110_v1.0.0")]
+    partial class v100
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
