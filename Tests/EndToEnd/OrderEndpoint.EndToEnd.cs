b663aa6eb991c0c56ddead0eee598b64aaf8b8de
https-richardy <code.richardy@gmail.com>
2024-11-05 13:39:31 -0300

    this commit adds tests that ensure the correct behavior of the API when fetching request details.

--- Tests/EndToEnd/OrderEndpoint.EndToEnd.cs
+++ Tests/EndToEnd/OrderEndpoint.EndToEnd.cs
@@ -104,6 +104,100 @@ public sealed class OrderEndpointTests :
         Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
     }
 
+    [Fact(DisplayName = "Given a valid order ID, when fetching order details, then it must return the corresponding order")]
+    public async Task GivenAValidOrderIdWhenFetchingOrderDetailsThenItMustReturnTheCorrespondingOrder()
+    {
+        // arrange: obtaining the necessary services
+        var services = _factory.GetServiceProvider();
+        var dbContext = services.GetRequiredService<ComandaDbContext>();
+
+        // arrange: creating an order
+        var order = _fixture.Create<Order>();
+
+        await dbContext.Orders.AddAsync(order);
+        await dbContext.SaveChangesAsync();
+
+        // arrange: authenticate httpClient as administrator
+        var authenticatedClient = await _factory.AuthenticateClientAsync(new AuthenticationCredentials
+        {
+            Email = "comanda@admin.com",
+            Password = "ComandaAdministrator123*"
+        });
+
+        var response = await authenticatedClient.GetAsync($"api/orders/{order.Id}");
+        var content = await response.Content.ReadFromJsonAsync<Response<FormattedOrderDetails>>();
+
+        response.EnsureSuccessStatusCode();
+
+        Assert.NotNull(content);
+        Assert.NotNull(content.Data);
+
+        Assert.Equal(order.Id, content.Data.Id);
+        Assert.Equal(order.Status, content.Data.Status);
+        Assert.Equal(order.Total, content.Data.Total);
+        Assert.Equal(order.Date, content.Data.Date);
+        Assert.Equal(order.CustomerName, content.Data.Customer);
+        Assert.Equal(order.Items.Count, content.Data.Items.Count);
+
+        Assert.NotNull(content.Data.Items);
+        Assert.NotEmpty(content.Data.Items);
+
+        Assert.NotNull(content.Data.ShippingAddress);
+        Assert.False(string.IsNullOrWhiteSpace(content.Data.ShippingAddress));
+    }
+
+    [Fact(DisplayName = "Should not allow anonymous and authenticated customers to retrieve order details")]
+    public async Task ShouldNotAllowAnonymousAndAuthenticatedCustomersToRetrieveOrderDetails()
+    {
+        // arrange: authenticate httpClient as customer
+        var authenticatedClient = await _factory.AuthenticateClientAsync(new AuthenticationCredentials
+        {
+            Email = "john.doe@email.com",
+            Password = "JohnDoe1234*"
+        });
+
+        // act and assert: send a request to obtain the order details
+        var response = await authenticatedClient.GetAsync($"api/orders/999");
+
+        Assert.Equal(HttpStatusCode.Forbidden, response.StatusCode);
+
+        // arrange: set httpClient to be anonymous
+        var anonymousClient = _factory.CreateClient();
+
+        // act and assert: send a request to obtain the order details
+        response = await anonymousClient.GetAsync($"api/orders/999");
+
+        Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+    }
+
+    [Fact(DisplayName = "Given a valid order ID, when fetching order details with a non-existing ID, then it must return 404")]
+    public async Task GivenAValidOrderIdWhenFetchingOrderDetailsWithNonExistingIdThenItMustReturn404()
+    {
+        // arrange: obtaining the necessary services
+        var services = _factory.GetServiceProvider();
+        var dbContext = services.GetRequiredService<ComandaDbContext>();
+
+        // arrange: creating an order
+        var order = _fixture.Create<Order>();
+
+        await dbContext.Orders.AddAsync(order);
+        await dbContext.SaveChangesAsync();
+
+        // arrange: authenticate httpClient as administrator
+        var authenticatedClient = await _factory.AuthenticateClientAsync(new AuthenticationCredentials
+        {
+            Email = "comanda@admin.com",
+            Password = "ComandaAdministrator123*"
+        });
+
+        // act: fetch order details with a non-existing ID
+        var nonExistingOrderId = 999;
+        var response = await authenticatedClient.GetAsync($"api/orders/{nonExistingOrderId}");
+
+        // assert: verify that the response status code is 404
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+
     public async Task DisposeAsync() => await Task.CompletedTask;
     public async Task InitializeAsync()
     {
