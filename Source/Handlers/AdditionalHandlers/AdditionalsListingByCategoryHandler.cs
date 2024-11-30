8b882d294c84fbf47668314c93319363b3ac0885
https-richardy <code.richardy@gmail.com>
2024-11-29 21:26:41 -0300

    this commit introduces data formatting for the additionals listing handler according to category, formatting so that only the relevant data is serialized in the API response.

--- Source/Handlers/AdditionalHandlers/AdditionalsListingByCategoryHandler.cs
+++ Source/Handlers/AdditionalHandlers/AdditionalsListingByCategoryHandler.cs
@@ -3,25 +3,30 @@ namespace Comanda.WebApi.Handlers;
 public sealed class AdditionalsListingByCategoryHandler(
     IAdditionalRepository additionalRepository,
     ICategoryRepository categoryRepository
-) : IRequestHandler<AdditionalsListingByCategoryRequest, Response<IEnumerable<Additional>>>
+) : IRequestHandler<AdditionalsListingByCategoryRequest, Response<IEnumerable<FormattedAdditional>>>
 {
-    public async Task<Response<IEnumerable<Additional>>> Handle(
+    public async Task<Response<IEnumerable<FormattedAdditional>>> Handle(
         AdditionalsListingByCategoryRequest request,
         CancellationToken cancellationToken
     )
     {
         var category = await categoryRepository.RetrieveByIdAsync(request.CategoryId);
         if (category is null)
-            return new Response<IEnumerable<Additional>>(
+            return new Response<IEnumerable<FormattedAdditional>>(
                 data: null,
                 statusCode: StatusCodes.Status404NotFound,
                 message: "Category not found."
             );
 
-        var additionals = await additionalRepository.FindAllAsync(additional => additional.Category.Id == request.CategoryId);
+        var additionals = await additionalRepository
+            .FindAllAsync(additional => additional.Category.Id == request.CategoryId);
 
-        return new Response<IEnumerable<Additional>>(
-            data: additionals,
+        var payload = additionals
+            .Select(additional => TinyMapper.Map<FormattedAdditional>(additional))
+            .ToList();
+
+        return new Response<IEnumerable<FormattedAdditional>>(
+            data: payload,
             statusCode: StatusCodes.Status200OK,
             message: "Additionals retrieved successfully."
         );
--- Source/Payloads/Requests/AdditionalPayloads/AdditionalsListingByCategoryRequest.cs
+++ Source/Payloads/Requests/AdditionalPayloads/AdditionalsListingByCategoryRequest.cs
@@ -1,7 +1,7 @@
 namespace Comanda.WebApi.Payloads;
 
 public sealed record AdditionalsListingByCategoryRequest
-    : IRequest<Response<IEnumerable<Additional>>>
+    : IRequest<Response<IEnumerable<FormattedAdditional>>>
 {
     public int CategoryId { get; init; }
 }
