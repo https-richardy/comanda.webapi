1aea8ae2c97ba0d06227c9a1c45ee1fec0d68ff5
https-richardy <code.richardy@gmail.com>
2024-12-10 17:06:09 -0300

    this refactoring replaces the direct use of `ICategoryRepository` with `ICategoryManager` in the category deletion handler.

--- Source/Handlers/CategoryHandlers/CategoryDeletionHandler.cs
+++ Source/Handlers/CategoryHandlers/CategoryDeletionHandler.cs
@@ -1,26 +1,32 @@
 namespace Comanda.WebApi.Handlers;
 
-public sealed class CategoryDeletionHandler(
-    ICategoryRepository categoryRepository,
-    ILogger<CategoryDeletionHandler> logger
-) : IRequestHandler<CategoryDeletionRequest, Response>
+public sealed class CategoryDeletionHandler :
+    IRequestHandler<CategoryDeletionRequest, Response>
 {
-    private readonly ICategoryRepository _repository = categoryRepository;
-    private readonly ILogger _logger = logger;
+    private readonly ICategoryManager _categoryManager;
+    private readonly ILogger _logger;
+
+    public CategoryDeletionHandler(
+        ICategoryManager categoryManager,
+        ILogger<CategoryDeletionHandler> logger
+    )
+    {
+        _categoryManager = categoryManager;
+        _logger = logger;
+    }
 
     public async Task<Response> Handle(
         CategoryDeletionRequest request,
-        CancellationToken cancellationToken
-    )
+        CancellationToken cancellationToken)
     {
-        var category = await _repository.RetrieveByIdAsync(request.CategoryId);
+        var category = await _categoryManager.GetAsync(request.CategoryId);
         if (category is null)
             return new Response(
                 statusCode: StatusCodes.Status404NotFound,
                 message: "Category not found."
             );
 
-        await _repository.DeleteAsync(category);
+        await _categoryManager.DeleteAsync(category);
         _logger.LogInformation("Category '{Title}' deleted successfully.", category.Name);
 
         return new Response(
