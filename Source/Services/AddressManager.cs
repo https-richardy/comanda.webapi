445b564c1392e4e6af16c64f43b4839b7871ef1d
https-richardy <code.richardy@gmail.com>
2024-12-10 18:28:30 -0300

    it implements the `AddressService` service for managing addresses, including methods for obtaining addresses by ID, searching by ZIP code via the external API (ViaCEP), creating, updating and deleting addresses.

---
+++ Source/Services/AddressManager.cs
@@ -0,0 +1,50 @@
+namespace Comanda.WebApi.Services;
+
+public sealed class AddressService : IAddressManager
+{
+    public IQueryable<Address> Addresses => _repository.Entities;
+
+    private readonly IAddressRepository _repository;
+    private readonly HttpClient _httpClient;
+
+    public AddressService(IAddressRepository repository, HttpClient httpClient)
+    {
+        _repository = repository;
+        _httpClient = httpClient;
+    }
+
+    public async Task<Address?> GetAddressByIdAsync(int id)
+    {
+        return await _repository.RetrieveByIdAsync(id);
+    }
+
+    public async Task<Address?> FetchAddressByZipCodeAsync(string zipCode)
+    {
+        zipCode = RemoveNonDigits(zipCode);
+
+        var response = await _httpClient.GetFromJsonAsync<ViaCepResponse>(requestUri: $"/ws/{zipCode}/json/");
+        var address = TinyMapper.Map<Address>(response);
+
+        return address;
+    }
+
+    public async Task CreateAddressAsync(Address address)
+    {
+        await _repository.SaveAsync(address);
+    }
+
+    public async Task UpdateAddressAsync(Address address)
+    {
+        await _repository.UpdateAsync(address);
+    }
+
+    public async Task DeleteAddressAsync(Address address)
+    {
+        await _repository.DeleteAsync(address);
+    }
+
+    private string RemoveNonDigits(string zipCode)
+    {
+       return zipCode.Replace("-", "").Trim();
+    }
+}
