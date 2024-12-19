119a48d386ae919ea7fa38e8361ade2a6f51c9b2
https-richardy <code.richardy@gmail.com>
2024-12-19 13:46:53 -0300

    this commit improves `NotificationHub` by adding authentication for logged-in users.  Unauthenticated users will have their connections aborted.  Administrators will be automatically added to the “Administrators” group when they connect, allowing notifications to be sent to this group.  The `AssociateOrder` and `SendOrderStatusUpdateAsync` methods have been removed

--- Source/Hubs/NotificationHub.cs
+++ Source/Hubs/NotificationHub.cs
@@ -7,16 +7,18 @@ public sealed class NotificationHub : Hub
         await Clients.All.SendAsync("receiveNotification", notification);
     }
 
-    public async Task AssociateOrder(int orderId)
+    public override async Task OnConnectedAsync()
     {
-        await Groups.AddToGroupAsync(Context.ConnectionId, orderId.ToString());
-        await Clients.Caller.SendAsync("associated", $"You are now associated with order {orderId}");
-    }
+        var user = Context.User;
+        if (user is null)
+        {
+            Context.Abort();
+            return;
+        }
 
-    public async Task SendOrderStatusUpdateAsync(int orderId, Order updatedOrder)
-    {
-        await Clients
-            .Group(orderId.ToString())
-            .SendAsync("receiveOrderStatusUpdate", updatedOrder);
+        if (user.IsInRole("Administrator"))
+            await Groups.AddToGroupAsync(Context.ConnectionId, "Administrators");
+
+        await base.OnConnectedAsync();
     }
 }
