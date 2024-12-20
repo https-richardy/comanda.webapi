dbc58709abf507739b154ae45464f106b7b1c16d
https-richardy <code.richardy@gmail.com>
2024-12-19 21:00:08 -0300

    this change adds maximum length validations for the “Title” and “Description” fields in the `ProductCreationValidator` validator.  The title must now have a maximum of 100 characters and the description a maximum of 120.  In addition, a validation has been implemented to ensure that the price is less than 1000.

--- Source/Validators/ProductValidators/ProductCreationValidator.cs
+++ Source/Validators/ProductValidators/ProductCreationValidator.cs
@@ -6,14 +6,17 @@ public sealed class ProductCreationValidator : AbstractValidator<ProductCreation
     {
         RuleFor(product => product.Title)
             .NotEmpty().WithMessage("Title is required.")
-            .MinimumLength(3).WithMessage("Title must be at least 3 characters.");
+            .MinimumLength(3).WithMessage("Title must be at least 3 characters.")
+            .MaximumLength(100).WithMessage("Title must be at most 100 characters.");
 
         RuleFor(product => product.Description)
             .NotEmpty().WithMessage("Description is required.")
-            .MinimumLength(10).WithMessage("Description must be at least 10 characters.");
+            .MinimumLength(10).WithMessage("Description must be at least 10 characters.")
+            .MaximumLength(120).WithMessage("Description must be at most 120 characters.");
 
         RuleFor(product => product.Price)
-            .GreaterThan(0).WithMessage("Price must be greater than 0.");
+            .GreaterThan(0).WithMessage("Price must be greater than 0.")
+            .LessThan(1000).WithMessage("Price must be less than 1000.");
 
         RuleFor(product => product.CategoryId)
             .GreaterThan(0).WithMessage("Category ID must be greater than 0.");
