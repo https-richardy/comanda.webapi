d8516c2e513a1da824ea9afeecee6cb39e24a4e6
https-richardy <code.richardy@gmail.com>
2024-12-19 21:27:48 -0300

    improved validations for creating and editing add-ons: This change adds minimum and maximum length validations for the name (between 3 and 50 characters) and limits the price between 0 and 1000, both for creating and editing add-ons.

--- Source/Validators/AdditionalValidators/AdditionalCreationValidator.cs
+++ Source/Validators/AdditionalValidators/AdditionalCreationValidator.cs
@@ -7,11 +7,14 @@ public sealed class AdditionalCreationValidator :
     public AdditionalCreationValidator()
     {
         RuleFor(additional => additional.Name)
-            .NotEmpty().WithMessage("Name is required.");
+            .NotEmpty().WithMessage("Name is required.")
+            .MinimumLength(3).WithMessage("Name must be at least 3 characters.")
+            .MaximumLength(50).WithMessage("Name must be at most 50 characters.");
 
         RuleFor(additional => additional.Price)
             .NotEmpty().WithMessage("Price is required.")
-            .GreaterThan(0).WithMessage("Price must be greater than zero.");
+            .GreaterThan(0).WithMessage("Price must be greater than zero.")
+            .LessThan(1000).WithMessage("Price must be less than 1000.");
 
         RuleFor(additional => additional.CategoryId)
             .GreaterThan(0).WithMessage("Category id must be greater than zero.");
