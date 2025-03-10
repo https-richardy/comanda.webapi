6ede4bcec96d087c3fd13fab685119a768c9b377
Richard Garcia <125691152+https-richardy@users.noreply.github.com>
2025-03-10 11:23:58 -0300

    this commit removes the `HandleSuccessfulPaymentAsync` and `HandleCanceledPaymentAsync` controller actions from the `CheckoutController`.

--- Source/Controllers/CheckoutController.cs
+++ Source/Controllers/CheckoutController.cs
@@ -11,18 +11,4 @@ public sealed class CheckoutController(IMediator mediator) : ControllerBase
         var response = await mediator.Send(request);
         return StatusCode(response.StatusCode, response);
     }
-
-    [HttpGet("success")]
-    public async Task<IActionResult> HandleSuccessfulPaymentAsync([FromQuery] SuccessfulPaymentRequest request)
-    {
-        var response = await mediator.Send(request);
-        return StatusCode(response.StatusCode, response);
-    }
-
-    [HttpGet("cancel")]
-    public async Task<IActionResult> HandleCanceledPaymentAsync([FromQuery] CanceledPaymentRequest request)
-    {
-        var response = await mediator.Send(request);
-        return StatusCode(response.StatusCode, response);
-    }
 }
