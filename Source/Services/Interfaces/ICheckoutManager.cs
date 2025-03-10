16ae2a4152bc379aaf829cf1f720c372035fdfef
Richard Garcia <125691152+https-richardy@users.noreply.github.com>
2025-03-10 01:30:50 -0300

    this change refactors the `CreateCheckoutSessionAsync` method in the `ICheckoutManager` interface. The return type has been changed from `Task<Session>` to `Task<CheckoutResponse>`

--- Source/Services/Interfaces/ICheckoutManager.cs
+++ Source/Services/Interfaces/ICheckoutManager.cs
@@ -2,6 +2,6 @@ namespace Comanda.WebApi.Services;
 
 public interface ICheckoutManager
 {
-    Task<Session> CreateCheckoutSessionAsync(Cart cart, Address address);
+    Task<CheckoutResponse> CreateCheckoutSessionAsync(Cart cart, Address address);
     Task<Session> GetSessionAsync(string sessionId);
 }
