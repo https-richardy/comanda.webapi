a48a6e04a9907e91f891a5f11c1a6583b6378d4d
Richard Garcia <125691152+https-richardy@users.noreply.github.com>
2025-03-10 10:56:29 -0300

    this change adds the `Customer` parameter to the `CreateCheckoutSessionAsync` method in the `ICheckoutManager` interface.  The method now receives information from the customer (`Cart`, `Customer`, `Address`) to create a checkout session, allowing for better management and customization of the purchasing process.

--- Source/Services/Interfaces/ICheckoutManager.cs
+++ Source/Services/Interfaces/ICheckoutManager.cs
@@ -2,6 +2,5 @@ namespace Comanda.WebApi.Services;
 
 public interface ICheckoutManager
 {
-    Task<CheckoutResponse> CreateCheckoutSessionAsync(Cart cart, Address address);
-    Task<Session> GetSessionAsync(string sessionId);
+    Task<CheckoutResponse> CreateCheckoutSessionAsync(Cart cart, Customer customer, Address address);
 }
